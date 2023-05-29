cd70edccac25e6e1911a9e8232401f26e9141267
ministrov <61511030+ministrov@users.noreply.github.com>
2023-05-29 11:42:07 +0300

    feat: end up operating with Home page component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
 import './styles/main.css';
 
 import Navbar from './components/navbar/Navbar';
-import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-
       <Navbar/>
-
-      <Header/>
-
-      <main className="section">
-        <div className="container">
-
-          <ul className="content-list">
-            <li className="content-list__item">
-              <h2 className="title-2">Frontend</h2>
-              <p>JavaScript, ReactJS, HTML, CSS, NPM, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents</p>
-            </li>
-            <li className="content-list__item">
-              <h2 className="title-2">Backend</h2>
-              <p>NodeJS, MySQL, MongoDB, PHP, Laravel</p>
-            </li>
-          </ul>
-
-        </div>
-      </main>
-
+      <Home />
       <Footer/>
     </div>
   );
