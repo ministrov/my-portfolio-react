d9a294f6836cc2bd6b08dda14c3140bab1c41718
unknown <ministrov2018@gmail.com>
2023-11-18 14:06:32 +0300

    working on fixing home page component

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -15,9 +15,49 @@ const About = () => {
       <div className="container">
         <h2 className="about__title title-2">About</h2>
 
-        {/* <div className="about__time-line time-line" style={TIME_LINE}>
-          <h3>Time line section</h3>
-        </div> */}
+        <section className="about-section">
+          <div className="about-section__bottom">
+            <motion.div
+              className="about-section__left"
+              initial={{ x: -50, opacity: 1 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              transition={{ type: 'tween', duration: 1 }}
+            >
+              
+              <p className="about-section__description">
+                Hi, I'm Anton!
+                <br />
+                <br />
+                I am a frontend developer with 3 years of experience. I specialize in creating functional and beautiful interfaces for web applications. My job is to help the customer realize his idea and make the product user-friendly.
+                <br />
+                <br />
+                To achieve this goal, I use the most modern technologies and tools. I also pay great attention to communication with the team and the customer in order to understand their needs and offer optimal solutions.
+                I will be glad to discuss your project and contribute to its success.portfolio, I showcase a range of projects that demonstrate my technical proficiency, design skills, and ability to work collaboratively with teams to achieve project goals.
+              </p>
+
+              
+
+
+
+              <div className="about-section__button-block">
+                <Button href={"#!"} text={"Read more >"} />
+              </div>
+            </motion.div>
+
+            <motion.div
+              className="about-section__right"
+              initial={{ x: 50, opacity: 1 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              transition={{ type: 'tween', duration: 1 }}
+            >
+              <img
+                className="about-section__image"
+                src={myPicture}
+                alt="Focus on author's face"
+              />
+            </motion.div>
+          </div>
+        </section>
 
         <motion.p
           className="about__descr"
@@ -43,49 +83,6 @@ const About = () => {
           deadlines, quickly assimilate new information, attentive to details.
         </motion.p>
 
-        <section className="about-section">
-          <div className="about-section__bottom">
-            <motion.div
-              className="about-section__left"
-              initial={{ x: -50, opacity: 1 }}
-              whileInView={{ x: 0, opacity: 1 }}
-              transition={{ type: 'tween', duration: 1 }}
-            >
-              <div className="about-section__title-block">
-                <div className="about-section__title-wrapper">
-                  <h2 className="about-section__title title-1">#about-me</h2>
-                  <div className="about-section__title-line">--------------</div>
-                </div>
-              </div>
-              <p className="about-section__description">
-                Hi, I'm Anton!
-                <br />
-                <br />
-                I'm a junior frontend developer with a passion for creating beautiful and functional websites. With a year and a half of experience in the industry, I have honed my skills in technical development, design principles, user experience, and effective communication.
-                <br />
-                <br />
-                Through my work, I strive to create engaging and intuitive web experiences that delight users and drive business results. In this portfolio, I showcase a range of projects that demonstrate my technical proficiency, design skills, and ability to work collaboratively with teams to achieve project goals.
-              </p>
-
-              <div className="about-section__button-block">
-                <Button href={"#!"} text={"Read more >"} />
-              </div>
-            </motion.div>
-
-            <motion.div
-              className="about-section__right"
-              initial={{ x: 50, opacity: 1 }}
-              whileInView={{ x: 0, opacity: 1 }}
-              transition={{ type: 'tween', duration: 1 }}
-            >
-              <img
-                className="about-section__image"
-                src={myPicture}
-                alt="Focus on author's face"
-              />
-            </motion.div>
-          </div>
-        </section>
       </div>
 
       <Video />
