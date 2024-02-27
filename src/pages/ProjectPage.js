26b84ce368f411a53ea80dc4484b036fbc71c8ff
unknown <ministrov2018@gmail.com>
2024-02-27 19:37:28 +0300

    feat: end up operating on refactoring project-card component, skillComponent, ProjectPage

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -1,13 +1,19 @@
 import { useParams } from 'react-router-dom';
 import Heading from '../components/heading/Heading';
 import Button from '../components/button/Button';
-import ButtonGitHub from '../components/buttonGitHub/ButtonGitHub';
+import SkillComponet from '../components/skillComponent/SkillComponent';
+// import ButtonGitHub from "../components/buttonGitHub/ButtonGitHub";
+// import SkillComponet from "../components/skillComponent/SkillComponent";
 import { projects } from '../helpers/projectsList';
 import { motion } from 'framer-motion';
 
-const Project = () => {
-  const {id} = useParams();
+
+
+const ProjectPage = () => {
+  const { id } = useParams();
   const project = projects[id];
+
+  console.log(typeof project);
 
   return (
     <motion.main
@@ -18,25 +24,49 @@ const Project = () => {
       <h1 className="visually-hidden">Page about single author's project</h1>
       <div className="container">
         <div className="project-details">
-          <Button text={"Back to Projects"} className={"project-details__btn btn--theme btn--med"} href={'/projects'} />
+          <Button
+            text={"Back to Projects"}
+            className={"project-details__btn btn--theme btn--med"}
+            href={"/projects"}
+          />
           <Heading title={project.title} slogan={project.slogan} />
-          <img src={project.imgBig} alt={project.title} className="project-details__cover" />
-          <p className="project-detail__overview">
-            Project Overview
-          </p>
+          <img
+            src={project.imgBig}
+            alt={project.title}
+            className="project-details__cover"
+          />
+          <div className="project-details__content">
+            <h3 className="project-detail__overview">Project Overview</h3>
 
-          <p className="project-detail__text-overview">{project.overview}</p>
+            <p className="project-detail__text-overview">{project.overview}</p>
 
-          <div className="project-details__desc">
-            <p>Skills: {project.skills}</p>
+            <p className="project-detail__text-overview">{project.overview}</p>
+
+            <h3 className="project-detail__overview">Tools Used</h3>
+
+            <ul className="project-detail__tools-list">
+              {project.skills.split(', ').map(item => (
+                <SkillComponet key={item} skillName={item} />
+              ))}
+            </ul>
+
+            <h3 className="project-detail__overview">See Live</h3>
+
+            {/* <li key={item} className="project-detail__tools-item">{console.log(typeof item)}</li> */}
+
+            {/* <div className="project-details__desc">
+              <p>Skills: {project.skills}</p>
+            </div> */}
+            <div className="project-details__links">
+              <Button text={'Live Link'} href={"/"} className={'project-detail__btn btn--med btn--theme'} />
+              <Button text={'Code Link'} href={"/"} className={'project-detail__btn btn--med btn--theme-inv'}/>
+            </div>
+            {/* {project.gitHubLink && <ButtonGitHub link="https://github.com" />} */}
           </div>
-          {project.gitHubLink && (
-            <ButtonGitHub link="https://github.com" />
-          )}
         </div>
       </div>
     </motion.main>
-  )
-}
+  );
+};
 
-export default Project;
+export default ProjectPage;
