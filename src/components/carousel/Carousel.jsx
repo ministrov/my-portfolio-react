5a47694711dfb72118fb98983e032b1c9e2d3446
unknown <ministrov2018@gmail.com>
2024-03-19 15:54:04 +0300

    feat: add swiper slider to carousel component, fixing burger menu and end up operating with adaptive

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,40 +1,68 @@
 import { useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import TestimonialCard from "../testimonialCard/TestimonialCard";
 import ControlLeft from "./controlLeft/ControlLeft";
 import ControlRight from "./controlRight/ControlRight";
 import "./style.css";
 
 const Carousel = ({ className }) => {
-  const slider = useRef(null);
-
-  const moveSlideLeft = () => {
-    slider.current?.scrollBy(-334, 0);
-  };
-
-  const moveSlideRight = () => {
-    slider.current?.scrollBy(334, 0);
+  const progressCircle = useRef(null);
+  const progressContent = useRef(null);
+  const onAutoplayTimeLeft = (s, time, progress) => {
+    progressCircle.current.style.setProperty('--progress', 1 - progress);
+    progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
   };
 
   return (
-    <div className={`${className} carousel`}>
-      <div ref={slider} className="carousel__inner">
-        <TestimonialCard />
-        <TestimonialCard />
-        <TestimonialCard />
-        <TestimonialCard />
-        <TestimonialCard />
-        <TestimonialCard />
-        <TestimonialCard />
-        <TestimonialCard />
+    <Swiper 
+      className={`${className} carousel`}
+    >
+      <div 
+        className="carousel__inner"
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        onAutoplayTimeLeft={onAutoplayTimeLeft}
+      >
+        <SwiperSlide>
+          <TestimonialCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <TestimonialCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <TestimonialCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <TestimonialCard />
+        </SwiperSlide>
+
+        <div className="autoplay-progress" slot="container-end">
+          <svg viewBox="0 0 48 48" ref={progressCircle}>
+            <circle cx="24" cy="24" r="20"></circle>
+          </svg>
+          <span ref={progressContent}></span>
+        </div>
       </div>
       <div className="carousel__controls">
-        <ControlLeft className="carousel__btn_prev" moveLeft={moveSlideLeft} />
+        <ControlLeft className="carousel__btn_prev" 
+          // moveLeft={moveSlideLeft} 
+        />
         <ControlRight
           className="carousel__btn_next"
-          moveRight={moveSlideRight}
+          // moveRight={moveSlideRight}
         />
       </div>
-    </div>
+    </Swiper>
   );
 };
 
