7e84a180deddd73e93c4c7e0f03154392aa63163
ministrov <61511030+ministrov@users.noreply.github.com>
2024-06-03 18:39:00 +0300

    feat: create a Faq component only with jsx markdown

---
+++ src/sections/faq/Faq.jsx
@@ -0,0 +1,15 @@
+import "./style.css";
+
+const Faq = () => {
+  return (
+    <section className="faq">
+      <div className="container">
+        <div className="faq__wrapper">
+          {'Faq'.toUpperCase()}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Faq
