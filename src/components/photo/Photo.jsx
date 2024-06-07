ece97916b1633fdca9ee00690e03d883dd55ad92
ministrov <61511030+ministrov@users.noreply.github.com>
2024-06-07 19:09:48 +0300

    feat: end up operating on photo component

--- src/components/photo/Photo.jsx
+++ src/components/photo/Photo.jsx
@@ -1,13 +1,60 @@
+import { motion } from "framer-motion";
 import myPicture from "../../assets/photo.png";
 import "./style.css";
 
 const Photo = () => {
   return (
-    <img
-      className="about__image"
-      src={myPicture}
-      alt="Focus on author's face"
-    />
+    <div className="about__image-block">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { delay: 2, duration: 0.4, ease: "easeIn"}
+        }}
+      >
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" }
+          }}
+          className="about__image-wrapper"
+        >
+          <img
+            className="about__image"
+            src={myPicture}
+            alt="Focus on author's face"
+          />
+        </motion.div>
+
+        {/* <motion.svg
+          className="about__svg"
+          fill="transparent"
+          viewBox="0 0 506 506"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.circle
+            cx="273"
+            cy="273"
+            r="275"
+            stroke="#0062b9"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{ strokeDasharray: "24 10 0 0"}}
+            animate={{
+              strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
+              rotate: [120, 360]
+            }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              repeatType: "reverse"
+            }}
+          />
+        </motion.svg> */}
+      </motion.div>
+    </div>
   )
 }
 
