b6d0334697b7ec8185e5ac18afe00981fa9472a7
ministrov <61511030+ministrov@users.noreply.github.com>
2024-06-13 13:37:18 +0300

    feat: add Lesson component to proctice TransitionGroup animation

--- src/sections/testimonials/Testimonials.jsx
+++ src/sections/testimonials/Testimonials.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Carousel from "../../components/carousel/Carousel";
 import Heading from "../../components/heading/Heading";
 import "./style.css";
@@ -16,7 +17,35 @@ const Testimonials = () => {
 
         <Carousel className="testimonials__carousel" />
       </div>
+
+      <Lesson/>
     </section>
+  );
+};
+
+const Lesson = () => {
+  const [isLogoVisible, setIsLogoVisible] = useState(false);
+
+  const toggleLogo = () => {
+    setIsLogoVisible((current) => !current);
+  };
+
+  return (
+    <div className="wrapper">
+      <div>
+        <h2>Do you want to see React Logo?</h2>
+        <input type="radio" name="logo" checked={isLogoVisible} onChange={toggleLogo} />
+        Yes
+        <input type="radio" name="logo" checked={isLogoVisible} onChange={toggleLogo} />
+        No
+      </div>
+
+      {isLogoVisible && (
+        <div>
+          {/* <img src={avatar} alt="Some picture in focus"/> */}
+        </div>
+      )}
+    </div>
   );
 };
 
