0687e1293b617060920d5ccbbe925ceadfe65ea2
unknown <ministrov2018@gmail.com>
2024-10-02 17:36:56 +0300

    fix: fixing skills icons and icons styling

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,5 +1,7 @@
-import { FaReact, FaNode, FaCss3Alt, FaVuejs } from 'react-icons/fa';
+import { FaReact, FaCss3Alt } from 'react-icons/fa';
 import { SiJavascript } from 'react-icons/si';
+import { SiTypescript } from "react-icons/si";
+import { SiRedux } from "react-icons/si";
 import { ImHtmlFive } from 'react-icons/im';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -7,31 +9,41 @@ export const skills = [
   {
     id: uuidv4(),
     tech: 'HTML',
-    icon: <ImHtmlFive />
+    icon: <ImHtmlFive color='red' />
   },
   {
     id: uuidv4(),
     tech: 'CSS3',
-    icon: <FaCss3Alt />
-  },
-  {
-    id: uuidv4(),
-    tech: 'React JS',
-    icon: <FaReact/>
-  },
-  {
-    id: uuidv4(),
-    tech: 'Vue3 JS',
-    icon: <FaVuejs />
-  },
-  {
-    id: uuidv4(),
-    tech: 'Node JS',
-    icon: <FaNode/>
+    icon: <FaCss3Alt color='blue' />
   },
   {
     id: uuidv4(),
     tech: 'JavaScript',
-    icon: <SiJavascript />
+    icon: <SiJavascript color='orange' />
+  },
+  {
+    id: uuidv4(),
+    tech: 'TypeScript',
+    icon: <SiTypescript color='blue' />
+  },
+  {
+    id: uuidv4(),
+    tech: 'React JS',
+    icon: <FaReact  color='lightblue'/>
+  },
+  {
+    id: uuidv4(),
+    tech: 'Redux',
+    icon: <SiRedux color='purple'/>
   }
+  // {
+  //   id: uuidv4(),
+  //   tech: 'ReduxToolkit',
+  //   icon: <SiRedux color='purple'/>
+  // },
+  // {
+  //   id: uuidv4(),
+  //   tech: 'Redux Toolkit + RTK Query',
+  //   icon: <SiRedux color='purple'/>
+  // }
 ];
