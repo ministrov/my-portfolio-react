48a383793a95e39fde6f819ad4df1da27dd41013
Zhila <ministrov2018@gmail.com>
2025-01-20 14:23:42 +0300

    feature - Turn on debounceResize function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import { BrowserRouter as Router } from "react-router-dom";
+
+import { debounce, onResize } from './utils/helperFunctions';
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import AnimatedRoutes from "./pages/AnimatedRoutes";
 import "./styles/main.css";
+
+const debounceResize = debounce(onResize, 500);
+
+debounceResize();
+debounceResize();
 
 const App = () => {
   return (
