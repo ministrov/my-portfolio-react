6036ad0ad7f6afa26fcf222ee8e16000b7f13d4a
Zhila <ministrov2018@gmail.com>
2025-01-21 12:14:08 +0300

    feature - Add util function to get directions

--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -11,6 +11,23 @@ function onResize() {
     console.log('Window screen size was changed');
 }
 
+function getDirection(direction) {
+    switch (direction) {
+        case 'left':
+            return 1;
+        case 'right':
+            return 2;
+        case 'up':
+            return 3;
+        case 'down':
+            return 4;
+        default:
+            return -1;
+    }
+}
+
+console.log(getDirection('left'));
+
 export {
     debounce,
     onResize
