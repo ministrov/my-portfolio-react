a77b788c4eb0fba813b7a9f70ebf40bcab072567
Zhila <ministrov2018@gmail.com>
2025-01-22 14:00:01 +0300

    feature - Add more responsive behavior for section services at all devices

--- src/sections/services/Services.jsx
+++ src/sections/services/Services.jsx
@@ -10,7 +10,8 @@ const Services = () => {
   
   return (
     <section className="services">
-      <motion.div
+      <div className="container">
+        <motion.div
           initial={{ opacity: 0 }}
           animate={{
             opacity: 1,
@@ -19,29 +20,30 @@ const Services = () => {
           className="services__list"
         >
           {services.map((service, index) => (
-            <div key={index}
-              className="services__item"
-            >
-              <div className="services__item-block">
-                <div className="services__item-text text-outline">{service.num}</div>
-                <Link className="services__item-link" href={service.href}>
-                  <BsArrowDownRight className="services__item-icon"/>
-                </Link>
+              <div key={index}
+                className="services__item"
+              >
+                <div className="services__item-block">
+                  <div className="services__item-text text-outline">{service.num}</div>
+                  <Link className="services__item-link" href={service.href}>
+                    <BsArrowDownRight className="services__item-icon"/>
+                  </Link>
+                </div>
+
+                <h2 className="services__subheading">{service.title}</h2>
+
+                <p className={`services__description ${isShort ? 'services__description--long' : ''}`}>{service.description}</p>
+
+                <button className="services__more" onClick={() => setIsShort((current) => !current)}>
+                  {isShort ? 'Hide all' : 'Show more'}
+                </button>
+
+                <div className="services__border"></div>
               </div>
-
-              <h2 className="services__subheading">{service.title}</h2>
-
-              <p className={`services__description ${isShort ? 'services__description--long' : ''}`}>{service.description}</p>
-
-              <button className="services__more" onClick={() => setIsShort((current) => !current)}>
-                {isShort ? 'Hide all' : 'Show more'}
-              </button>
-
-              <div className="services__border"></div>
-            </div>
-          ))
+            ))
           }
         </motion.div>
+      </div>
     </section>
   )
 }
