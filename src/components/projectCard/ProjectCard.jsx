2c5936bb9cae7a96ea0a2478e9920e24012e18fb
Zhila <ministrov2018@gmail.com>
2025-01-24 13:24:39 +0300

    fix - Fixing project card hover and focusing styling

--- src/components/projectCard/ProjectCard.jsx
+++ src/components/projectCard/ProjectCard.jsx
@@ -3,8 +3,8 @@ import "./style.css";
 
 const ProjectCard = ({ title, img, index, imageAlt }) => {
   return (
-    <NavLink to={`/project/${index}`} tabIndex={0}>
-      <article className="project__card project-card">
+    <NavLink to={`/project/${index}`}>
+      <article className="project__card project-card"  tabIndex={0}>
         <li className="project-card__item">
           <img src={img} className="project-card__image" width={338} height={224} alt={imageAlt} />
           <h3 className="project-card__title">{title}</h3>
