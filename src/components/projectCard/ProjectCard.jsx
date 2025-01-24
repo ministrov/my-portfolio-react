8c7c4cd91777146c8db258bc4e4b6b9338675193
Zhila <ministrov2018@gmail.com>
2025-01-24 12:36:22 +0300

    fix - Fixing animation scaling on project card component

--- src/components/projectCard/ProjectCard.jsx
+++ src/components/projectCard/ProjectCard.jsx
@@ -3,14 +3,14 @@ import "./style.css";
 
 const ProjectCard = ({ title, img, index, imageAlt }) => {
   return (
-    <article className="project__card project-card">
-      <NavLink to={`/project/${index}`}>
+    <NavLink to={`/project/${index}`} tabIndex={0}>
+      <article className="project__card project-card">
         <li className="project-card__item">
           <img src={img} className="project-card__image" width={338} height={224} alt={imageAlt} />
           <h3 className="project-card__title">{title}</h3>
         </li>
-      </NavLink>
-    </article>
+      </article>
+    </NavLink>
   );
 };
 
