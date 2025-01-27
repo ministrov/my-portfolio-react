539e20149ab0000ddef81da747114daede4cd841
Zhila <ministrov2018@gmail.com>
2025-01-27 18:38:53 +0300

    feature - Create a checkbox component to check liked or not

--- src/components/myCheckBox/MyCheckBox.jsx
+++ src/components/myCheckBox/MyCheckBox.jsx
@@ -1,8 +1,26 @@
+import { useState } from 'react';
 import './style.css';
 
 const MyCheckBox = () => {
+  const [liked, setLiked] = useState(false);
+
+  function handleChange(e) {
+    setLiked(e.target.checked);
+  }
   return (
-    <div className='checkbox'>MyCheckBox</div>
+    <div className='checkbox'>
+      <label>
+        <input 
+          type="checkbox"
+          checked={liked}
+          onChange={handleChange}
+        />
+
+        I liked this
+      </label>
+
+      <p> You {liked ? 'liked': 'did not like'} this.</p>
+    </div>
   )
 }
 
