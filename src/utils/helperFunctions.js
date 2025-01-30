849fc0ad738ae0de77b2f8682d42060b8b4ac5c1
Zhila <ministrov2018@gmail.com>
2025-01-30 16:10:29 +0300

    feature - Write and add a helper function to replace string

--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -48,11 +48,50 @@ const sumTwo = (nums, target) => {
 // Input: nums = [2, 7, 11, 15], target = 9
 // Output: [0, 1];
 
+function replaceSubstringImperativeWay(str, search, replace) {
+    let result = '';
+
+    for (let i = 0; i < str.length; i++) {
+        if (str.substring(i, i + search.length) === search) {
+            result += replace;
+            i += search.length - 1;
+        } else {
+            result += str[i];
+        }
+    }
+
+    return result;
+}
+
+console.log(replaceSubstringImperativeWay('hello world', 'world', 'there'))
+
 function replateSubstring(str, search, replace) {
     return str.split(search).join(replace);
 }
 
 console.log(replateSubstring('hello world', 'world', 'there'));
+
+function isPalindrome(str) {
+    const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+
+    return cleaned === cleaned.split('').reverse().join('');
+}
+
+console.log(isPalindrome('hello'));
+
+function isPalindromeImperativeWay(str) {
+    let cleanedStr = str.replace(/[^a-z0-9]/g, '').toLowerCase();
+
+    for (let i = 0; i < Math.floor(cleanedStr.length / 2); i++) {
+        if (cleanedStr[i] !== cleanedStr[cleanedStr.length - 1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(isPalindromeImperativeWay('hello'));
 
 export {
     debounce,
