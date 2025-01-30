1018784010f7b6bb2b95c3ddf6ea20a67acdfe4d
Zhila <ministrov2018@gmail.com>
2025-01-30 16:23:14 +0300

    feature - Write and add realisation for myFilter methor from the prototype of the Array

--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -93,6 +93,24 @@ function isPalindromeImperativeWay(str) {
 
 console.log(isPalindromeImperativeWay('hello'));
 
+// eslint-disable-next-line no-extend-native
+Array.prototype.myFilter = function(callback) {
+    const result = [];
+
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+            result.push(this[i]);
+        }
+    }
+
+    return result;
+}
+
+const arr = [1, 2, 3, 4, 5];
+const filteredArr = arr.myFilter(x => x > 2);
+
+console.log(filteredArr);
+
 export {
     debounce,
     onResize,
