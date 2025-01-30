c15e19537b03708043f4d87a7d946fd6f0886993
Zhila <ministrov2018@gmail.com>
2025-01-30 12:29:57 +0300

    feature - Start realisation of text animation with Framer motion in section promo

--- src/components/typingText/TypingText.jsx
+++ src/components/typingText/TypingText.jsx
@@ -1,19 +1,6 @@
-// import { useState, useEffect, useRef } from 'react';
 import "./style.css";
 
 const TypingText = ({ className, text }) => {
-  // const [currentText, setCurrentText] = useState('');
-  // const intervalRef = useRef(null);
-
-  // useEffect(() => {
-  // intervalRef.current = setInterval(() => {
-  //   setCurrentText(prevText => prevText + text[currentText.length]);
-  //   if (currentText.length >= text.length) clearInterval(intervalRef.current);
-  // }, 100);
-
-  //     return () => clearInterval(intervalRef.current);
-  // }, [text, currentText.length]);
-
   return (
     <strong className={className}>{text}</strong>
   )
---
+++ src/utils/stplitStringRegExp.js
@@ -0,0 +1,13 @@
+export function spliteStringRegExp(string) {
+    const characters = [];
+    const regex = /[\s\S]/gu;
+
+    let match;
+
+    while ((match = regex.exec(string)) !== null) {
+        console.log(match);
+        characters.push(match[0]);
+    }
+
+    return characters;
+}
