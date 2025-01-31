7d9ba1e1cdbefbaafda548e067f45c95f5e793c0
Zhila <ministrov2018@gmail.com>
2025-01-31 11:37:00 +0300

    fix - Fixing heading hyrarchy to faq section component

--- src/sections/faq/Faq.jsx
+++ src/sections/faq/Faq.jsx
@@ -8,6 +8,7 @@ const Faq = () => {
   const [ activeIndex, setActiveIndex ] = useState(-1);
   return (
     <section className="faq">
+      <h2 className="visually-hidden">Frequantly Asked Questions section</h2>
       <div className="container">
         <Heading 
           title={"Frequantly Asked Questions"} 
@@ -27,7 +28,7 @@ const Faq = () => {
                 }}
               >
                 <div className="faq__question">
-                  <h3>{item.title}</h3>
+                  <h4>{item.title}</h4>
                   {
                     activeIndex === index ?
                       <div className="faq__icon">
