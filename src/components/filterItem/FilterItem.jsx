8ddc2e4cacc40e49634caa8500d219bec032144b
Zhila <ministrov2018@gmail.com>
2025-01-31 12:56:50 +0300

    fix - Deleting filter items component

---
+++ src/components/filterItem/FilterItem.jsx
@@ -0,0 +1,16 @@
+// import './style.css';
+
+// const FilterItem = () => {
+//   return (
+//     <li className={`projects__filter-list-item`}>
+//         <FilterButton
+//             active={active}
+//             currentBtn={'All'}
+//             filterName={'All'}
+//             onClick={() => handleFilterClick("All")}
+//         />
+//     </li>
+//   )
+// }
+
+// export default FilterItem;
