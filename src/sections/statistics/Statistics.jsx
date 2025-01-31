24d104ae971175ee9a6deecd6d81a00af1226722
Zhila <ministrov2018@gmail.com>
2025-01-31 11:26:32 +0300

    refactore - Decomposite statistics section list item to component

--- src/sections/statistics/Statistics.jsx
+++ src/sections/statistics/Statistics.jsx
@@ -1,4 +1,4 @@
-import CountUp from 'react-countup';
+import StatisticListItem from '../../components/statisticsListItem/StatisticListItem.jsx';
 import { statistics } from '../../data/stats.js';
 import './style.css';
 
@@ -7,21 +7,14 @@ const Statistics = () => {
     <section className="statistics">
       <h2 className="visually-hidden">Section for a Statistics</h2>
       <div className="container">
-        <div className="statistics__list">
+        <ul className="statistics__list">
           {statistics.map((stats, index) => (
-            <div className="statistics__item" key={index + 1}>
-              <CountUp
-                end={stats.num}
-                duration={5}
-                delay={2}
-                className="statistics__count"
-              />
-              <p className={`statistics__desc ${stats.text.length < 15 ? "max-width-100" : "max-width-150"}`}>
-                {stats.text}
-              </p>
-            </div>
+            <StatisticListItem
+              key={index + 1} 
+              stats={stats}
+            />
           ))}
-        </div>
+        </ul>
       </div>
     </section>
   )
