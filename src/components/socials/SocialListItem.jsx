4c3d22e15830b753720dd6a40bb0434b1a34e4c1
Zhila <ministrov2018@gmail.com>
2025-02-03 13:36:57 +0300

    fix - Improving accessibilty for social link component

--- src/components/socials/SocialListItem.jsx
+++ src/components/socials/SocialListItem.jsx
@@ -4,9 +4,9 @@ import "./style.css";
 const SocialListItem = ({ social }) => {
   return (
     <li className="socials__item">
-        <Link to={social.path} target='_blank' className="socials__link">
+      <Link to={social.path} target="_blank" aria-label="Go to link path" className="socials__link">
         {social.icon}
-        </Link>
+      </Link>
     </li>
   )
 }
