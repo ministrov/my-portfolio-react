8ac41c24b88db6a1a23f113cd29476dd84a5adce
Zhila <ministrov2018@gmail.com>
2025-02-03 17:24:21 +0300

    refactore - Start refactore app component to organize routes properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
-import AnimatedRoutes from "./pages/AnimatedRoutes";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollToTop from './components/scrollToTop/ScrollToTop';
+import HomePage from "./pages/HomePage";
+import ProjectsPage from "./pages/ProjectsPage";
+import ProjectPage from "./pages/ProjectPage";
+import Page404 from "./pages/Page404";
+// import Header from "./components/header/Header";
+// import Footer from "./components/footer/Footer";
 import "./styles/main.css";
+// import ScrollToTop from './components/scrollToTop/ScrollToTop';
 
 const App = () => {
   return (
-    <Router>
-      <Header />
-      <AnimatedRoutes />
-      <Footer />
-    </Router>
+    <BrowserRouter>
+      <ScrollToTop/>
+      <Routes >
+        <Route path="/" element={<HomePage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/project/:id" element={<ProjectPage />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+      {/* <Header />
+      <Footer /> */}
+    </BrowserRouter>
   );
 };
 
--- src/pages/AnimatedRoutes.jsx
+++ src/pages/AnimatedRoutes.jsx
@@ -1,23 +1,26 @@
-import { Route, Routes, useLocation } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
-import Page404 from "./Page404";
-import HomePage from "./HomePage";
-import ProjectsPage from "./ProjectsPage";
-import ProjectPage from "./ProjectPage";
+// import { Route, Routes } from "react-router-dom";
+// import { AnimatePresence } from "framer-motion";
+// import Page404 from "./Page404";
+// import HomePage from "./HomePage";
+// import ProjectsPage from "./ProjectsPage";
+// import ProjectPage from "./ProjectPage";
+// import ScrollToTop from '../components/scrollToTop/ScrollToTop';
 
-const AnimatedRoutes = () => {
-  const location = useLocation();
+// const AnimatedRoutes = () => {
+//   // const location = useLocation();
+//   // location={location} key={location.pathname}
 
-  return (
-    <AnimatePresence>
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/projects" element={<ProjectsPage />} />
-        <Route path="/project/:id" element={<ProjectPage />} />
-        <Route path="*" element={<Page404 />} />
-      </Routes>
-    </AnimatePresence>
-  );
-};
+//   return (
+//     <AnimatePresence>
+//       <ScrollToTop/>
+//       <Routes >
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="/projects" element={<ProjectsPage />} />
+//         <Route path="/project/:id" element={<ProjectPage />} />
+//         <Route path="*" element={<Page404 />} />
+//       </Routes>
+//     </AnimatePresence>
+//   );
+// };
 
-export default AnimatedRoutes;
+// export default AnimatedRoutes;
