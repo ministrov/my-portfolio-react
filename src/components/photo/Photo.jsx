23428b40e3ddaa62f2c1bd815f85595018a9586b
Zhila <ministrov2018@gmail.com>
2025-02-04 17:10:14 +0300

    refactore - Rebuild photo component to imporve

--- src/components/photo/Photo.jsx
+++ src/components/photo/Photo.jsx
@@ -1,27 +1,35 @@
 import { motion } from "framer-motion";
-// import myPicture from "../../assets/png/my-photo.png";
-import myPicture from "../../assets/png/photo.png";
+import Image from '../image/Image';
+import authorPhoto from "../../assets/png/photo.png";
+import webPPhoto from "../../assets/png/photo.webp";
 import "./style.css";
 
 const Photo = () => {
   return (
-    <div className="about__image-block">
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{
-          opacity: 1,
-          transition: { delay: 2, duration: 0.4, ease: "easeIn"}
-        }}
-      >
-        <img
+    <motion.div
+      className="about__image-block"
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2, duration: 0.4, ease: "easeIn"}
+      }}
+    >
+      <Image 
+        className="about__image"
+        width={500}
+        height={500}
+        fallback={authorPhoto}
+        src={webPPhoto}
+        alt={"Focus on author's face"}
+      />
+        {/* <img
           className="about__image"
           width={500}
           height={500}
-          src={myPicture}
+          src={authorPhoto}
           alt="Focus on author's face"
-        /> 
-      </motion.div>
-    </div>
+        />  */}
+    </motion.div>
   )
 }
 
