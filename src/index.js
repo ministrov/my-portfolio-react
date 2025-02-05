80ed343a531332be0095bf9f7e0d672bbdbb1e6f
Zhila <ministrov2018@gmail.com>
2025-02-05 13:11:44 +0300

    fix - Comment all unusual helper functions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from 'react-helmet-async';
+// import { deb } from './utils/helperFunctions';
 import App from "./App";
 import 'swiper/css';
 import "swiper/css/bundle";
+
+// console.log(deb());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
