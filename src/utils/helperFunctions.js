961bca82f6a9b7110586a553cebb2097194de12f
Zhila <ministrov2018@gmail.com>
2025-02-07 11:16:11 +0300

    feature - Add a new one helper function to convert roman letters to integers

--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -46,6 +46,35 @@ const isPalindromeNum = function (num) {
 
 console.log(isPalindromeNum(232));
 
+const romanToInt = function (string) {
+    let result = 0, current = 0, previous = 0;
+    const values = new Map([
+        ['I', 1],
+        ['V', 5],
+        ['X', 10],
+        ['L', 50],
+        ['C', 100],
+        ['D', 500],
+        ['M', 1000],
+    ]);
+
+    for (const char of string.split('').reverse()) {
+        current = values.get(char);
+
+        if (current >= previous) {
+            result += current;
+        } else {
+            result -= current;
+        }
+
+        previous = current;
+    }
+
+    return result;
+}
+
+console.log(romanToInt('IV'));
+
 // Input: nums = [2, 7, 11, 15], target = 9
 // Output: [0, 1];
 
