02f3a0872a04304472f4c5cf802eb91763fcc55b
Zhila <ministrov2018@gmail.com>
2025-02-07 10:39:55 +0300

    feature - Add sumTwo helper function new way of realisation

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import { Provider } from 'react-redux';
 import { HelmetProvider } from 'react-helmet-async';
 import App from "./App";
 import { store } from './srore/store';
-import { sumTwo } from './utils/helperFunctions';
+// import { sumTwo } from './utils/helperFunctions';
 import "swiper/css";
 import "swiper/css/bundle";
 
-console.log(sumTwo([2, 7, 11, 15], 6));
+// console.log(sumTwo([2, 7, 11, 15], 6));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -13,17 +13,36 @@ const sumTwo = (nums, target) => {
     return [targetFirstIndex, targetLastIndex];
 }
 
-// var twoSum = function (nums, target) {
-//     for (let i = 0; i < nums.length; i++) {
-//         for (let j = i + 1; j < nums.length; j++) {
-//             if (nums[j] === target - nums[i]) {
-//                 return [i, j];
-//             }
-//         }
-//     }
-//     // Return an empty array if no solution is found
-//     return [];
-// };
+var twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[j] === target - nums[i]) {
+                return [i, j];
+            }
+        }
+    }
+    // Return an empty array if no solution is found
+    return [];
+};
+
+console.log(twoSum);
+
+const isPalindromeNum = function (num) {
+    const s = String(num);
+    let l = 0, reverse = s.length - 1;
+
+    while (l <= reverse) {
+        if (s[l] !== s[reverse]) {
+            return false;
+        }
+        l++;
+        reverse--;
+    }
+
+    return true;
+}
+
+console.log(isPalindromeNum(232));
 
 // Input: nums = [2, 7, 11, 15], target = 9
 // Output: [0, 1];
