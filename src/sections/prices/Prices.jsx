6e72c4325b523960b0bf50c862dddc9de3d91105
Zhila <ministrov2018@gmail.com>
2025-02-24 13:35:21 +0300

    feature - Create a prices section component

---
+++ src/sections/prices/Prices.jsx
@@ -0,0 +1,14 @@
+import Heading from '../../components/heading/Heading';
+import "./style.css";
+
+const Prices = () => {
+  return (
+    <section className="prices">
+      <div className="container">
+        <Heading title={"Prices"} slogan={"Give you a best prices for professional web developing"} />
+      </div>
+    </section>
+  )
+}
+
+export default Prices;
