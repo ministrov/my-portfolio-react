511c0e482170f571e331d5ddb78d58bb7c22d45c
Zhila <ministrov2018@gmail.com>
2025-02-24 13:43:29 +0300

    feature - Create a prices list and item components

---
+++ src/components/pricesList/PricesLIst.jsx
@@ -0,0 +1,12 @@
+// import { v4 as uuidv4 } from 'uuid';
+import "./style.css";
+
+const PricesList = () => {
+  return (
+    <ul className="prices__list">
+      
+    </ul>
+  )
+}
+
+export default PricesList;
--- src/sections/prices/Prices.jsx
+++ src/sections/prices/Prices.jsx
@@ -6,6 +6,8 @@ const Prices = () => {
     <section className="prices">
       <div className="container">
         <Heading title={"Prices"} slogan={"Give you a best prices for professional web developing"} />
+
+        {/* Pricing card list */}
       </div>
     </section>
   )
