8ac264efd418c200c72d4f4002bc2d01558f94ca
Zhila <ministrov2018@gmail.com>
2025-03-15 11:30:35 +0300

    feature - Operating on adding an animation to serviecesListItem

--- src/components/servicesListItem/ServicesListItem.jsx
+++ src/components/servicesListItem/ServicesListItem.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react';
+// import { motion } from 'framer-motion';
 import { BsArrowDownRight } from "react-icons/bs";
 import './style.css';
 
 const ServicesListItem = ({ service }) => {
     const [isShort, setIsShort] = useState(false);
+
+    //  const variants = {
+    //     short: { opacity: 1, height: 180 },
+    //     long: { opacity: 0, height: 'auto', maxHeight: 'max-content' }
+    // };
 
     return (
         <li
@@ -21,10 +27,10 @@ const ServicesListItem = ({ service }) => {
             <p className={`services__description ${isShort ? 'services__description--long' : ''}`}>{service.description}</p>
 
             <button className="services__more" onClick={() => setIsShort((current) => !current)}>
-                {isShort ? 'Hide ever' : 'Show more'}
+                {isShort ? 'Hide' : 'Show more'}
             </button>
         </li>
     )
 }
 
-export default ServicesListItem
+export default ServicesListItem;
