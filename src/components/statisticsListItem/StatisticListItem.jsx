e9becd0c16ea3b14c02cc5d066c785da6824c94d
Zhila <ministrov2018@gmail.com>
2025-03-22 14:32:36 +0300

    fix - Fixing a small styling mistakes over all project

--- src/components/statisticsListItem/StatisticListItem.jsx
+++ src/components/statisticsListItem/StatisticListItem.jsx
@@ -4,15 +4,15 @@ import './style.css';
 const StatisticListItem = ({ stats }) => {
   return (
     <li className="statistics__item">
-        <CountUp
-            end={stats.num}
-            duration={5}
-            delay={2}
-            className="statistics__count"
-        />
-        <p className={`statistics__desc ${stats.text.length < 15 ? "max-width-100" : "max-width-150"}`}>
-        {stats.text}
-        </p>
+      <CountUp
+        end={stats.num}
+        duration={5}
+        delay={2}
+        className="statistics__count"
+      />
+      <p className={`statistics__desc ${stats.text.length < 15 ? "max-width-100" : "max-width-150"}`}>
+      {stats.text}
+      </p>
     </li>
   )
 }
