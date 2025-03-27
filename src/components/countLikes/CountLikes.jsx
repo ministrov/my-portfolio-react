ee0584e4b7f57c243090e64a615cb96a05b63292
Zhila <ministrov2018@gmail.com>
2025-03-27 11:12:22 +0300

    fix - Fixing an error in the store

--- src/components/countLikes/CountLikes.jsx
+++ src/components/countLikes/CountLikes.jsx
@@ -3,12 +3,14 @@ import { PiHeartbeatLight } from "react-icons/pi";
 import './style.css';
 
 const CountLikes = () => {
-    const likes = useSelector((state) => state.likes);
+    const likes = useSelector((state) => state);
+
+    console.log(likes);
 
     return (
         <div className="likes">
             <PiHeartbeatLight size={45} color="#0062b9"/>
-            <span className="likes__count">{`${likes.counter}`}</span>
+            <span className="likes__count">{likes}</span>
         </div>
     )
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { languageSlice } from './language.slice';
+// import { languageSlice } from './language.slice';
 import { likesCounterSlice } from './counter.slice';
 import { saveState } from './localStorage';
 import { logIt } from './middleware';
 
 export const store = configureStore({
     reducer: {
-        language: languageSlice,
+        // language: languageSlice,
         likes: likesCounterSlice
     },
     middleware: () => [logIt]
 });
+
+console.log(store.reducer);
 
 store.subscribe(() => {
     saveState({ name: store.getState().likes.counter }, 'likes');
