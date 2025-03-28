3d88103a3d02340aaddb562a71cc36ed474c31b6
Zhila <ministrov2018@gmail.com>
2025-03-28 15:44:55 +0300

    feature - Add an accessibility features to all project and checking Lighthouse DevTools Chrome

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from './Layout';
 import ScrollToTop from './components/scrollToTop/ScrollToTop';
 import HomePage from "./pages/HomePage";
-import ProjectsPage from "./pages/ProjectsPage";
-import ProjectPage from "./pages/projectPage/ProjectPage";
-import PageNotFound from "./pages/pageNotFound/PageNotFound";
+// import ProjectsPage from "./pages/ProjectsPage";
+// import ProjectPage from "./pages/projectPage/ProjectPage";
+// import PageNotFound from "./pages/pageNotFound/PageNotFound";
 import "./styles/main.css";
+
+const ProjectsPageLazy = lazy(() => import("./pages/ProjectsPage"));
+const ProjectPageLazy = lazy(() => import("./pages/projectPage/ProjectPage"));
+const PageNotFoundLazy = lazy(() => import("./pages/pageNotFound/PageNotFound"));
 
 const App = () => {
   return (
@@ -14,9 +19,12 @@ const App = () => {
       <Routes >
         <Route path='/' element={<Layout />}>
           <Route index element={<HomePage />} />
-          <Route path="/projects" element={<ProjectsPage />} />
-          <Route path="/project/:id" element={<ProjectPage />} />
-          <Route path="*" element={<PageNotFound />} />
+          {/* <Route path="/projects" element={<ProjectsPage />} /> */}
+          <Route path="/projects" element={<Suspense fallback={<>Loading...</>}><ProjectsPageLazy /></Suspense>} />
+          {/* <Route path="/project/:id" element={<ProjectPage />} /> */}
+          <Route path="/project/:id" element={<Suspense><ProjectPageLazy /></Suspense>} />
+          {/* <Route path="*" element={<PageNotFound />} /> */}
+          <Route path="*" element={<Suspense><PageNotFoundLazy /></Suspense>} />
         </Route>
       </Routes>
     </BrowserRouter>
