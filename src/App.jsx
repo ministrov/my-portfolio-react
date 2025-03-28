689e4e6d25a001f8d43d7dadf616fdd46cc9b09d
Zhila <ministrov2018@gmail.com>
2025-03-28 16:10:25 +0300

    fix - Fixing a little mistakes over the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { lazy, Suspense } from 'react';
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from './Layout';
+import Layout from "./Layout";
 import ScrollToTop from './components/scrollToTop/ScrollToTop';
 import HomePage from "./pages/HomePage";
-// import ProjectsPage from "./pages/ProjectsPage";
-// import ProjectPage from "./pages/projectPage/ProjectPage";
-// import PageNotFound from "./pages/pageNotFound/PageNotFound";
 import "./styles/main.css";
 
 const ProjectsPageLazy = lazy(() => import("./pages/ProjectsPage"));
@@ -19,11 +16,8 @@ const App = () => {
       <Routes >
         <Route path='/' element={<Layout />}>
           <Route index element={<HomePage />} />
-          {/* <Route path="/projects" element={<ProjectsPage />} /> */}
           <Route path="/projects" element={<Suspense fallback={<>Loading...</>}><ProjectsPageLazy /></Suspense>} />
-          {/* <Route path="/project/:id" element={<ProjectPage />} /> */}
           <Route path="/project/:id" element={<Suspense><ProjectPageLazy /></Suspense>} />
-          {/* <Route path="*" element={<PageNotFound />} /> */}
           <Route path="*" element={<Suspense><PageNotFoundLazy /></Suspense>} />
         </Route>
       </Routes>
