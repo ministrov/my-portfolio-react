19fcd1b7459d113530dce284f06a9b0215b3b0a6
Zhila <ministrov2018@gmail.com>
2025-03-31 19:00:57 +0300

    feature - Write a function to handle toggle button to toggle language

--- src/components/toggleLang/ToggleLang.jsx
+++ src/components/toggleLang/ToggleLang.jsx
@@ -1,12 +1,41 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setLanguage } from '../../store/language.slice';
+import i18n from 'i18next';
 import './styles.css';
 
-export const ToggleLang = () => {
-    const [toggled, setToggled] = useState(false);
+// export const ToggleLang = () => {
+//     const [toggled, setToggled] = useState(false);
 
+//     return (
+//         <button className={`toggle-btn ${toggled ? 'toggled' : ''}`} onClick={() => setToggled(!toggled)}>
+//             <div className="thumb">{toggled ? 'Ru' : 'En'}</div>
+//         </button>
+//     );
+// }
+
+export const ToggleLang = () => {
+    const dispatch = useDispatch();
+    const currentLang = useSelector((state) => state.language.currentLang);
+
+    const handleLanguageChange = (lang) => {
+        dispatch(setLanguage(lang));
+        i18n.changeLanguage(lang)
+    }
     return (
-        <button className={`toggle-btn ${toggled ? 'toggled' : ''}`} onClick={() => setToggled(!toggled)}>
-            <div className="thumb">{toggled ? 'Ru' : 'En'}</div>
-        </button>
+        <div className="toggle-btn">
+            <button
+                onClick={() => handleLanguageChange('en')}
+                disabled={currentLang === 'en'}
+            >
+                English
+            </button>
+            <button
+                onClick={() => handleLanguageChange('ru')}
+                disabled={currentLang === 'ru'}
+            >
+                Français
+            </button>
+        </div>
     );
 }
--- src/store/language.slice.js
+++ src/store/language.slice.js
@@ -1,18 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit';
-import i18n from '../utils/i18n';
+// import i18n from '../utils/i18n';
 
 const initialState = {
-  currentLanguage: i18n.language || 'en',
+  currentLanguage: 'en',
 };
 
 export const languageSlice  = createSlice({
     name: 'language',
     initialState,
     reducers: {
-        toggleLanguage: (state) => {
-            // state.currentLanguage
-        },
-        setLanguage: (state, action) => {}
+        toggleLanguage: (state, action) => {
+            state.currentLanguage = action.payload
+        }
+        // setLanguage: (state, action) => {}
     }
 });
 
