19aad293c4d59f5106fbfa04b6e8aa977338fd30
Zhila <ministrov2018@gmail.com>
2025-03-31 18:31:28 +0300

    feature - Add a language slice to set up a language state

--- src/store/language.slice.js
+++ src/store/language.slice.js
@@ -1,19 +1,21 @@
-// import { createSlice } from '@reduxjs/toolkit';
-// import i18n from '../utils/i18n';
+import { createSlice } from '@reduxjs/toolkit';
+import i18n from '../utils/i18n';
 
-// const initialState = {
-//   currentLanguage: i18n.language || 'en',
-// };
+const initialState = {
+  currentLanguage: i18n.language || 'en',
+};
 
-// export const languageSlice  = createSlice({
-//     name: 'language',
-//     initialState,
-//     reducers: {
-//         toggleLanguage: (state) => {},
-//         setLanguage: (state, action) => {}
-//     }
-// });
+export const languageSlice  = createSlice({
+    name: 'language',
+    initialState,
+    reducers: {
+        toggleLanguage: (state) => {
+            // state.currentLanguage
+        },
+        setLanguage: (state, action) => {}
+    }
+});
 
 
-// export const { toggleLanguage, setLanguage } = languageSlice.actions;
-// export default languageSlice.reducer;
+export const { toggleLanguage, setLanguage } = languageSlice.actions;
+export default languageSlice.reducer;
