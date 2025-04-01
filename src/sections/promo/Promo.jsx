5880ef6bbbb57a30d1ec83aa0cca58d613a65e12
Zhila <ministrov2018@gmail.com>
2025-04-01 15:22:15 +0300

    fix - Remove unusual feature from promo component

--- src/sections/promo/Promo.jsx
+++ src/sections/promo/Promo.jsx
@@ -1,5 +1,5 @@
-import { useDispatch } from "react-redux";
-import { incrementLike } from "../../store/counter.slice";
+// import { useDispatch } from "react-redux";
+// import { incrementLike } from "../../store/counter.slice";
 import TypingText from "../../components/typingText/TypingText";
 import Image from "../../components/image/Image";
 import Button from "../../components/button/Button";
@@ -10,14 +10,14 @@ import MyAvatar1 from "../../assets/png/my-avatar.webp";
 import "./style.css";
 
 const Promo = () => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   return (
     <section className="promo">
       <h2 className="visually-hidden">A promo section for introduction of the author</h2>
       <div
         className="promo__content"
       >
-        <TypingText className={'promo__title'} text={"Hey Folks, I'm Anton Zhilin, a Frontend Developer!"}/>
+        <TypingText className={'promo__title'} text={"Hey Folks, I'm Anton Zhilin, a Frontend Developer!"} />
 
         <p className="promo__text">
           Welcome to a world where pixels come alive and digital dreams become
@@ -31,21 +31,16 @@ const Promo = () => {
             href={require("../../assets/pdfs/my-cv.pdf")}
             className={"btn--big"}
           />
-          <Button
-            onClick={() => dispatch(incrementLike())}
-            text={"Give a like"}
-            className={"btn--big"}
-          />
         </div>
       </div>
       <div className="promo__image-wrapper">
-        <Image src={MyAvatar1} fallback={MyAvatar} width={252} height={252} alt={"A funny pixel men with a laptop in his hands"}/>
+        <Image src={MyAvatar1} fallback={MyAvatar} width={252} height={252} alt={"A funny pixel men with a laptop in his hands"} />
       </div>
 
       <MouseScroll />
 
       <div className="promo__socials">
-        <SocialList className={"socials__list"}/>
+        <SocialList className={"socials__list"} />
       </div>
     </section>
   );
--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import languageReducer from './language.slice';
-import likesCounterReducer from './counter.slice';
-import { saveState } from './localStorage';
 import { logIt } from './middleware';
 
 export const store = configureStore({
     reducer: {
-        language: languageReducer,
-        likes: likesCounterReducer
+        language: languageReducer
     },
     middleware: () => [logIt]
 });
 
-store.subscribe(() => {
-    saveState({ name: store.getState().likes.counter }, 'likes');
-});
