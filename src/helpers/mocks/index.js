c27d135675f5f83ba86fcd3077b6d8c80947a9fe
Zhila <ministrov2018@gmail.com>
2025-04-01 17:30:53 +0300

    feature - Translate a statistics section with a i18n library

--- src/helpers/mocks/index.js
+++ src/helpers/mocks/index.js
@@ -1,3 +1,6 @@
+// import { useTranslation } from 'react-i18next';
+// const { t } = useTranslation();
+
 export const faqs = [
   {
     title: "What services do you offer?",
@@ -51,9 +54,9 @@ export const filters = [
 ];
 
 export const prices = [
-    { id: 1, title: 'Promotional websites for marketing tasks', description: 'We develop short and long pages with responsiveness, fast loading speed, micro-animations, semantic structure, and SEO-friendly markup.', price: '25 $ / per hour' },
-    { id: 2, title: 'Online stores and corporate websites for businesses', description: 'We confidently handle high-traffic formats—pages with interactivity, forms, shopping carts. We always prioritize the user and create accessible interfaces.', price: '45 $ / per hour' },
-    { id: 3, title: 'Complex services and admin panels for product teams', description: 'We create media, aggregators, and charts. We work with a large number of pages with similar components, where there are many points of interaction with the user and complex logic.', price: '75 $ / per hour' },
+  { id: 1, title: 'Promotional websites for marketing tasks', description: 'We develop short and long pages with responsiveness, fast loading speed, micro-animations, semantic structure, and SEO-friendly markup.', price: '25 $ / per hour' },
+  { id: 2, title: 'Online stores and corporate websites for businesses', description: 'We confidently handle high-traffic formats—pages with interactivity, forms, shopping carts. We always prioritize the user and create accessible interfaces.', price: '45 $ / per hour' },
+  { id: 3, title: 'Complex services and admin panels for product teams', description: 'We create media, aggregators, and charts. We work with a large number of pages with similar components, where there are many points of interaction with the user and complex logic.', price: '75 $ / per hour' },
 ];
 
 export const statistics = [
@@ -64,9 +67,9 @@ export const statistics = [
 ];
 
 export const testimonials = [
-    { id: 1, content: "I was amazed by the exceptional quality of service and attention to detail—truly a top-notch experience!" },
-    { id: 2, content: "Their professionalism and dedication made all the difference. I couldn't be happier with the results." },
-    { id: 3, content: "The level of expertise and care they provided exceeded my expectations, highly recommended!" },
-    { id: 4, content: "The level of expertise and care they provided exceeded my expectations, highly recommended!" },
-    { id: 5, content: "The level of expertise and care they provided exceeded my expectations, highly recommended!" },
+  { id: 1, content: "I was amazed by the exceptional quality of service and attention to detail—truly a top-notch experience!" },
+  { id: 2, content: "Their professionalism and dedication made all the difference. I couldn't be happier with the results." },
+  { id: 3, content: "The level of expertise and care they provided exceeded my expectations, highly recommended!" },
+  { id: 4, content: "The level of expertise and care they provided exceeded my expectations, highly recommended!" },
+  { id: 5, content: "The level of expertise and care they provided exceeded my expectations, highly recommended!" },
 ];
