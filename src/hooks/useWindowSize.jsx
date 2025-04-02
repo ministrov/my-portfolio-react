e2a73d4b2f6f9d7bf81a55933cb0df3c2f759422
Zhila <ministrov2018@gmail.com>
2025-04-02 11:29:45 +0300

    feature - Create a custom hook for measure a window resizing

---
+++ src/hooks/useWindowSize.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+export const useWindowSize = () => {
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+  });
+
+  useEffect(() => {
+    // Обработчик события изменения размера окна
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+      });
+    }
+
+    // Добавляем обработчик события
+    window.addEventListener("resize", handleResize);
+
+    // Убираем обработчик события при размонтировании компонента
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Пустой массив зависимостей указывает на выполнение эффекта только при монтировании
+
+  return windowSize; // Возвращаем текущее значение размера окна
+};
--- src/sections/advertisement/Advertisement.jsx
+++ src/sections/advertisement/Advertisement.jsx
@@ -1,16 +1,25 @@
+import { useWindowSize } from "../../hooks/useWindowSize";
 import Heading from "../../components/heading/Heading";
 import "./style.css";
 
 const Advertisement = () => {
+  const windowSize = useWindowSize();
+
+  console.log(windowSize);
   return (
     <section className="advertisement">
       <div className="container">
-        <Heading title={"Advertisement"} slogan={"Give you the best place to your advertisement"} />
+        <Heading
+          title={"Advertisement"}
+          slogan={"Give you the best place to your advertisement"}
+        />
 
-        <div className="advertisement__advs">Your advertisement could be here.</div>
+        <div className="advertisement__advs">
+          Your advertisement could be here.
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Advertisement;
