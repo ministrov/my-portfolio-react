2fb64fe151f9d09bef368a2eeb75402181c88dfb
Zhila <ministrov2018@gmail.com>
2025-04-02 15:06:30 +0300

    feature - Practice to a ternary operator and conditioanl rendering

---
+++ src/utils/parentFunction.js
@@ -0,0 +1,18 @@
+// A ternary operator
+
+// let canVote;
+// const age = 18;
+
+// if (age >= 18) {
+//   canVote = "sfdsf";
+// } else {
+//   canVote = "fdfdsfsdfsdf";
+// }
+
+// console.log(canVote);
+
+// const canVote2 = age >= 18 ? "You can vote" : "You are too small";
+
+// console.log(canVote2);
+
+//
