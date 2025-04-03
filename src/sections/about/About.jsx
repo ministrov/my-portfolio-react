8eb715d2049730b0631b63f13d7f31cc51e57539
Zhila <ministrov2018@gmail.com>
2025-04-03 19:11:50 +0300

    feature - Translate an about section with i18n library

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -20,25 +20,14 @@ const About = () => {
             <p className="about__description">
               <TypingText
                 className={'about__greeting'}
-                text={'Greeting, new customers'}
+                text={t('about.title')}
               />
               <br />
               <br />
-              With several years spent weaving pixels and lines of code into
-              captivating experiences, I've honed my skills as a frontend
-              developer who transforms visions into interactive realities. My
-              job is to help the customer realize his idea and make the product
-              user-friendly.
+              {t('about.descriptionOne')}
               <br />
               <br />
-              To achieve this goal, I use the most modern technologies and
-              tools. I also pay great attention to communication with the team
-              and the customer in order to understand their needs and offer
-              optimal solutions. I will be glad to discuss your project and
-              contribute to its success.portfolio, I showcase a range of
-              projects that demonstrate my technical proficiency, design skills,
-              and ability to work collaboratively with teams to achieve project
-              goals.
+              {t('about.descriptionTwo')}
             </p>
           </div>
 
