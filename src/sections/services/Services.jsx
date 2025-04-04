e0e0e9d36afed1fa1c875f86ab227cca0949ccba
Zhila <ministrov2018@gmail.com>
2025-04-04 17:06:41 +0300

    feature - End up operating on translating services component

--- src/sections/services/Services.jsx
+++ src/sections/services/Services.jsx
@@ -1,4 +1,4 @@
-// import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import ServicesListItem from '../../components/servicesListItem/ServicesListItem';
 import { motion } from 'framer-motion';
 import { SlGlobe } from 'react-icons/sl';
@@ -7,30 +7,27 @@ import { TbSeo } from 'react-icons/tb';
 import './style.css';
 
 const Services = () => {
-  // const { t } = useTranslation();
+  const { t } = useTranslation();
 
   const services = [
     {
       num: '01',
-      title: 'Web Development',
-      description:
-        'I craft responsive and intuitive web interfaces using cutting-edge technologies and best practices. My goal is to create websites that not only look stunning but also perform flawlessly across all devices.',
+      title: t('services.title.one'),
+      description: t('services.description.one'),
       href: '',
       icon: <SlGlobe color="white" size={25} />,
     },
     {
       num: '02',
-      title: 'UI/UX Design',
-      description:
-        'I design user-centric interfaces that prioritize usability and aesthetics, ensuring a smooth and enjoyable user experience. My focus is on creating visually appealing designs that enhance engagement and simplify interactions.',
+      title: t('services.title.two'),
+      description: t('services.description.two'),
       href: '',
       icon: <SiAffinitydesigner color="white" size={25} />,
     },
     {
       num: '03',
-      title: 'SEO',
-      description:
-        'I optimize websites to improve search engine rankings, driving organic traffic and increasing visibility online. By implementing effective SEO strategies, I ensure your content reaches its target audience and achieves maximum impact.',
+      title: t('services.title.three'),
+      description: t('services.description.three'),
       href: '',
       icon: <TbSeo color="white" size={25} />,
     },
