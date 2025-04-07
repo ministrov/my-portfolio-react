76c2d533c1b7fd58aadf0c2ec3d6eae3ab34b516
Zhila <ministrov2018@gmail.com>
2025-04-07 18:12:48 +0300

    feature - End up translating on footer and all home page

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,31 +1,32 @@
+import { useTranslation } from 'react-i18next';
 import Logo from '../logo/Logo';
 import SocialList from '../socials/SocialList';
 import './style.css';
 
 const Footer = () => {
+  const { t } = useTranslation();
+
   return (
     <footer className="main-footer">
       <div className="container">
         <div className="main-footer__upper">
           <div className="main-footer__row main-footer__row-1">
             <h2 className="main-footer__heading-sm">
-              <span>Socials</span>
+              {/* <span>Socials</span> */}
+              {t('footer.socials')}
             </h2>
             <SocialList className={'main-footer__list'} />
           </div>
           <div className="main-footer__row main-footer__row-2">
             <Logo className={'main-footer'} type={'logo__name--second'} />
-            <p className="main-footer__short-desc">
-              Creating Intuitive User Experiences Through Clean Code and Modern
-              Design
-            </p>
+            <p className="main-footer__short-desc">{t('footer.description')}</p>
           </div>
         </div>
 
         <div className="main-footer__lower">
-          &copy; Copyright 2021. Made by
+          &copy; {t('footer.copyright')}
           <a rel="noreferrer" target="_blank" href="#!">
-            Anton Zhilin
+            {t('footer.author')}
           </a>
         </div>
       </div>
