2ecadfd25d0c07b848d5f477f7434bb7c703c38b
Zhila <ministrov2018@gmail.com>
2025-04-07 15:11:16 +0300

    feature - End up translating testimonials section

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,46 +1,36 @@
+import { useTranslation } from 'react-i18next';
 import { Swiper, SwiperSlide } from 'swiper/react';
-// import { Autoplay, EffectFade } from 'swiper/modules';
+import { Autoplay, EffectFade } from 'swiper/modules';
 import TestimonialCard from '../testimonialCard/TestimonialCard';
-// import { testimonials } from '../../helpers/mocks';
 import './style.css';
 
 const Carousel = ({ className }) => {
+  const { t } = useTranslation();
+
   const testimonials = [
     {
       id: 1,
-      content:
-        'I was amazed by the exceptional quality of service and attention to detail—truly a top-notch experience!',
+      content: t('testimonials.contentOne'),
     },
     {
       id: 2,
-      content:
-        "Their professionalism and dedication made all the difference. I couldn't be happier with the results.",
+      content: t('testimonials.contentTwo'),
     },
     {
       id: 3,
-      content:
-        'The level of expertise and care they provided exceeded my expectations, highly recommended!',
+      content: t('testimonials.contentThree'),
     },
-    // {
-    //   id: 4,
-    //   content:
-    //     'The level of expertise and care they provided exceeded my expectations, highly recommended!',
-    // },
-    // {
-    //   id: 5,
-    //   content:
-    //     'The level of expertise and care they provided exceeded my expectations, highly recommended!',
-    // },
   ];
+
   return (
     <Swiper
       className={`${className} carousel`}
       spaceBetween={30}
       centeredSlides={true}
-      // autoplay={{
-      //   delay: 2000,
-      // }}
-      // modules={[Autoplay, EffectFade]}
+      autoplay={{
+        delay: 2000,
+      }}
+      modules={[Autoplay, EffectFade]}
     >
       {testimonials.map((testimonial) => (
         <SwiperSlide key={testimonial.id}>
