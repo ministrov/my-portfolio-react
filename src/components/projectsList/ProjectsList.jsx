bab582f29b7d5f28be4973983b319ae9b22ae465
Zhila <ministrov2018@gmail.com>
2025-04-26 16:56:56 +0300

    feature - End up operating on translating a projects list with i18n

--- src/components/projectsList/ProjectsList.jsx
+++ src/components/projectsList/ProjectsList.jsx
@@ -1,14 +1,17 @@
+import { useTranslation } from 'react-i18next';
 import ProjectCard from '../projectCard/ProjectCard';
 import './style.css';
 
 const ProjectsList = ({ projects }) => {
+  const { t } = useTranslation();
+
   return (
     <ul className="projects__list">
       {projects.map((project, index) => (
         <ProjectCard
           key={project.id}
           id={project.id}
-          title={project.title}
+          title={t(project.title)}
           skills={project.skills.split(',')}
           img={project.img}
           wepImg={project.webpImg}
