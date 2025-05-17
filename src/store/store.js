fd916d21a55dc36dc03fb5338dd144314accdac7
Zhila <ministrov2018@gmail.com>
2025-05-17 09:45:02 +0300

    refactore - Remove store folder with all files

--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import languageReducer from './language.slice';
-
-// export const store = configureStore({
-//   reducer: {
-//     language: languageReducer,
-//   },
-// });
