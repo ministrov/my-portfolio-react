12d74be3c45e850d8e26ea4ec0fee0178a3bb6c1
Zhila <ministrov2018@gmail.com>
2025-05-17 09:38:15 +0300

    fix - Remove unusaal files

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import languageReducer from './language.slice';
-import { logIt } from './middleware';
 
 export const store = configureStore({
   reducer: {
     language: languageReducer,
   },
-  middleware: () => [logIt],
 });
