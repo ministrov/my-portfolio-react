c9a2be8bc97af180f4447e974bc057f845ad60a0
ministrov <61511030+ministrov@users.noreply.github.com>
2025-06-13 18:56:39 +0300

    refactore - End up refactoring on skillComponent

--- src/components/skillComponent/SkillComponent.jsx
+++ src/components/skillComponent/SkillComponent.jsx
@@ -4,17 +4,19 @@ import './style.css';
 const SkillComponet = ({ skill }) => {
   return (
     <li key={skill.id} className="skill__card" aria-label={`${skill.tech}`}>
-      <div>
+      <div className="skill__left">
         <div className="skill__card-icon">{skill.icon}</div>
         <span className="skill__card-header">{skill.tech}</span>
       </div>
 
-      <div>
+      <p className="skill__count">
         <CountUp
           end={skill.percent}
-        />
-        %
-      </div>
+          duration={5}
+          delay={2}
+          className="skill__percent-num"
+        />%
+      </p>
     </li>
   );
 };
