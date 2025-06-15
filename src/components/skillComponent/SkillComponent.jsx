7476eb6b20ff8295dded353ce2235c1999b375be
ministrov <61511030+ministrov@users.noreply.github.com>
2025-06-15 16:11:38 +0300

    refactore - Refactoring animation in Skills componet and rebuild skillComponent

--- src/components/skillComponent/SkillComponent.jsx
+++ src/components/skillComponent/SkillComponent.jsx
@@ -1,28 +1,11 @@
-import { motion } from 'framer-motion';
 import CountUp from 'react-countup';
 import './style.css';
 
-const variants = {
-  hidden: { opacity: 0 },
-  visible: (index) => ({
-    opacity: 1,
-    transition: { delay: index * 0.6 },
-  }),
-};
-
-const SkillComponet = ({ skill, custom }) => {
+const SkillComponet = ({ skill }) => {
   if (!skill) return null;
 
   return (
-    <motion.li
-      key={skill.id}
-      variants={variants}
-      initial={'hidden'}
-      animate={'visible'}
-      className="skill__card"
-      custom={custom}
-      aria-label={skill.tech}
-    >
+    <>
       <div className="skill__left">
         <div className="skill__card-icon">{skill.icon}</div>
         <span className="skill__card-header">{skill.tech}</span>
@@ -36,7 +19,7 @@ const SkillComponet = ({ skill, custom }) => {
           className="skill__percent-num"
         />%
       </p>
-    </motion.li>
+    </>
   );
 };
 
--- src/sections/skills/Skills.jsx
+++ src/sections/skills/Skills.jsx
@@ -1,8 +1,40 @@
 import { useTranslation } from 'react-i18next';
+import { motion } from 'framer-motion';
 import Heading from '../../components/heading/Heading';
 import SkillComponet from '../../components/skillComponent/SkillComponent';
 import { skills } from './skills';
 import './style.css';
+
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.5
+    }
+  }
+}
+
+const item = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 }
+}
+
+// return (
+//   <motion.ol
+//     variants={container}
+//     initial="hidden"
+//     animate="show"
+//   >
+//     <motion.li variants={item} />
+//     <motion.li variants={item} />
+//   </motion.ol>
+// )
+
+// const charVariants = {
+//   hidden: { opacity: 0 },
+//   reveal: { opacity: 1 },
+// };
 
 const Skills = () => {
   const { t } = useTranslation();
@@ -20,15 +52,27 @@ const Skills = () => {
           <div className="skills__left">
             <h3 className="skills__left-heading">{t('skills.titleOne')}</h3>
 
-            <ul className="skills__list">
-              {skills.map((skill, index) => (
-                <SkillComponet
+            <motion.ul
+              initial="hidden"
+              whileInView="show"
+              variants={container}
+              className="skills__list"
+            >
+              {skills.map((skill) => (
+                <motion.li
                   key={skill.id}
-                  custom={index}
-                  skill={skill}
-                />
+                  transition={{ duration: 0.5 }}
+                  variants={item}
+                  className="skill__card"
+                  aria-label={skill.tech}
+                >
+                  <SkillComponet
+                    key={skill.id}
+                    skill={skill}
+                  />
+                </motion.li>
               ))}
-            </ul>
+            </motion.ul>
           </div>
           <div className="skills__right">
             <h3 className="skills__right-heading">{t('skills.titleTwo')}</h3>
