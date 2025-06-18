edee0ae07d843959fd493ce4a1c0be63c713ab14
ministrov <61511030+ministrov@users.noreply.github.com>
2025-06-18 18:02:07 +0300

    refactore - Create a new component for one project page

---
+++ src/components/project/Project.jsx
@@ -0,0 +1,69 @@
+import { motion } from 'framer-motion';
+import { useTranslation } from 'react-i18next';
+import Image from '../image/Image';
+import './Project.css';
+
+const Project = ({ project }) => {
+  const { t } = useTranslation();
+
+  return (
+    <motion.div
+      className="project"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      data-testid="project"
+    >
+      <div className="project__content">
+        <div className="project__image">
+          <Image
+            src={project.wepImg}
+            fallback={project.img}
+            alt={project.imageAlt}
+            width={1200}
+            height={800}
+          />
+        </div>
+
+        <div className="project__details">
+          <h2 id="project__title">{project.title}</h2>
+          <p className="project__description">{project.description}</p>
+
+          <div className="project__technologies">
+            <h3>{t('project-technologies')}:</h3>
+            <div className="tags-container">
+              {project.skills.map((skill, index) => (
+                <span key={index} className="tech-tag">{skill}</span>
+              ))}
+            </div>
+          </div>
+
+          <div className="project__-links">
+            {/* {project.demoUrl && (
+                  <a
+                    href={project.demoUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="demo-link"
+                  >
+                    {t('projectproject__.liveDemo')}
+                  </a>
+                )}
+                {project.githubUrl && (
+                  <a
+                    href={project.githubUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="github-link"
+                  >
+                    GitHub
+                  </a>
+                )} */}
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Project;
