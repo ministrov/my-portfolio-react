2b6654d64b86dd03bd720d322b28421ff7ef2461
ministrov <61511030+ministrov@users.noreply.github.com>
2025-06-19 13:22:15 +0300

    refactore - End up operating on creating a new UI for project page, desktop version

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -27,21 +27,30 @@ const Project = ({ project }) => {
           />
         </div>
 
-        <div className="project__details nebo nebo--tl">
-          <h4>Project Info</h4>
+        <div className="project__details-wrap nebo nebo--tl">
+          {/* <h3 className="project-card__info-title">{title}</h3> */}
 
-          <div className="project__details-table">
-            <div className="project__details-table-row">
-              <span>Year</span>
-              <span>2023</span>
-            </div>
-            <div className="project__details-table-row">
-              <span>Role</span>
-              <span>Front-end Developer</span>
+          <p className="project__details-desc">
+            Teamed up with a designer to breathe life into a promotional webpage for our beloved show, Adventure Time. Delivered a fully responsive design with dynamic content capabilities, seamlessly integrating a newsletter feature to keep fans updated with the latest adventures.
+          </p>
+
+          <div className="project__details">
+            <h4>Project Info</h4>
+
+            <div className="project__details-table">
+              <div className="project__details-table-row">
+                <span>Year</span>
+                <span>2023</span>
+              </div>
+              <div className="project__details-table-row">
+                <span>Role</span>
+                <span>Front-end Developer</span>
+              </div>
             </div>
           </div>
-        </div>
+
           <div className="project__links"></div>
+        </div>
       </div>
     </motion.div>
   );
