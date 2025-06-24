e04b91a4ab14b74a89368637be46cdc2e8f99c34
ministrov <61511030+ministrov@users.noreply.github.com>
2025-06-24 14:37:48 +0300

    fix - Fixing a projects data structure to remove unusual images and property of object field

--- src/sections/projects/projects.js
+++ src/sections/projects/projects.js
@@ -1,31 +1,20 @@
-// import project01 from '../../assets/projects/yeticave.jpg';
-import project01Wep from '../../assets/projects/yeticave.webp';
-import project02 from '../../assets/projects/create-x-construction.jpg';
-import project02Wep from '../../assets/projects/create-x-construction.webp';
-import project03 from '../../assets/projects/nike-ecommerce.jpg';
-import project03Wep from '../../assets/projects/nike-ecommerce.webp';
-// import project04 from '../../assets/projects/personal-diary.jpg';
-import project04Wep from '../../assets/projects/personal-diary.webp';
-import project05 from '../../assets/projects/05-big.jpg';
-import project05Wep from '../../assets/projects/05-big.webp';
-import project06 from '../../assets/projects/dictionary.jpg';
-import project06Wep from '../../assets/projects/dictionary.webp';
-import project07 from '../../assets/projects/blog.jpg';
-import project07Wep from '../../assets/projects/blogWebp.webp';
-import project08 from '../../assets/projects/ratingus.jpg';
-import project08Wep from '../../assets/projects/ratingus.webp';
-import project09 from '../../assets/projects/waterdel.jpg';
-import project09Wep from '../../assets/projects/waterdel.webp';
-import project10 from '../../assets/projects/bringitup.jpg';
-import project10Wep from '../../assets/projects/bringitup.webp';
+import yeticave from '../../assets/projects/yeticave.webp';
+import createX from '../../assets/projects/create-x-construction.webp';
+import nikeEcommerce from '../../assets/projects/nike-ecommerce.webp';
+import personalDiary from '../../assets/projects/personal-diary.webp';
+import topApp from '../../assets/projects/05-big.webp';
+import dictionary from '../../assets/projects/dictionary.webp';
+import blog from '../../assets/projects/blogWebp.webp';
+import ratingus from '../../assets/projects/ratingus.webp';
+import waterdel from '../../assets/projects/waterdel.webp';
+import bringitup from '../../assets/projects/bringitup.webp';
 
 export const projects = [
   {
     id: 1,
     title: 'projects.project1.title',
     skills: 'JavaScript, PHP, HTML, CSS',
-    img: project01Wep,
-    webpImg: project01Wep,
+    img: yeticave,
     slogan: 'projects.project1.slogan',
     gitHubLink: 'https://github.com/ministrov/yetycave-php',
     overview: 'projects.project1.overview',
@@ -35,8 +24,7 @@ export const projects = [
     id: 2,
     title: 'projects.project2.title',
     skills: 'JavaScript, HTML5, CSS3',
-    img: project02,
-    webpImg: project02Wep,
+    img: createX,
     demoLink: 'https://create-x-construction.vercel.app/',
     slogan: 'projects.project2.slogan',
     overview: 'projects.project2.overview',
@@ -46,8 +34,7 @@ export const projects = [
     id: 3,
     title: 'projects.project3.title',
     skills: 'React, Tailwind CSS',
-    img: project03,
-    webpImg: project03Wep,
+    img: nikeEcommerce,
     gitHubLink: 'https://github.com/ministrov/nike-react-tailwilnd',
     slogan: 'projects.project3.slogan',
     overview: 'projects.project3.overview',
@@ -58,8 +45,7 @@ export const projects = [
     id: 4,
     title: 'projects.project4.title',
     skills: 'React, React-id-Generator, classnames',
-    img: project04Wep,
-    webpImg: project04Wep,
+    img: personalDiary,
     slogan: 'projects.project4.slogan',
     gitHubLink: 'https://github.com/ministrov/react-journal',
     overview: 'projects.project4.overview',
@@ -69,8 +55,7 @@ export const projects = [
     id: 5,
     title: 'projects.project5.title',
     skills: 'Next, TypeScript, React, Framer Motion',
-    img: project05,
-    webpImg: project05Wep,
+    img: topApp,
     slogan: 'projects.project5.slogan',
     gitHubLink: 'https://github.com/ministrov/top-app-next-ts',
     overview: 'projects.project5.overview',
@@ -80,8 +65,7 @@ export const projects = [
     id: 6,
     title: 'projects.project6.title',
     skills: 'Next, TypeScript, React, Zustand, Yup, Formik, Framer Motion',
-    img: project08,
-    webpImg: project08Wep,
+    img: ratingus,
     slogan: 'projects.project6.slogan',
     gitHubLink: 'https://github.com/PurpleSchoolPractice/ratingus-site',
     demoLink: 'https://ratingus.ru/',
@@ -92,8 +76,7 @@ export const projects = [
     id: 7,
     title: 'projects.project7.title',
     skills: 'Next, TypeScript, React, Framer Motion',
-    img: project07,
-    webpImg: project07Wep,
+    img: blog,
     slogan: 'projects.project7.slogan',
     gitHubLink: 'https://github.com/ministrov/personal-blog-next-ts',
     overview: 'projects.project7.overview',
@@ -103,8 +86,7 @@ export const projects = [
     id: 8,
     title: 'projects.project8.title',
     skills: 'JavaScript, HTML, CSS',
-    img: project06,
-    webpImg: project06Wep,
+    img: dictionary,
     slogan: 'projects.project8.slogan',
     overview: 'projects.project8.overview',
     isProduction: false,
@@ -113,8 +95,7 @@ export const projects = [
     id: 9,
     title: 'projects.project9.title',
     skills: 'JavaScript, HTML, CSS',
-    img: project09,
-    webpImg: project09Wep,
+    img: waterdel,
     slogan: 'projects.project9.slogan',
     overview: 'projects.project9.overview',
     demoLink: 'https://waterdel.ru/',
@@ -124,8 +105,7 @@ export const projects = [
     id: 10,
     title: 'projects.project10.title',
     skills: 'JavaScript, HTML, CSS, OOP, SOLID',
-    img: project10,
-    webpImg: project10Wep,
+    img: bringitup,
     slogan: 'projects.project10.slogan',
     overview: 'projects.project10.overview',
     isProduction: false,
