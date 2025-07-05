3de8c408d8036e2bdfe7426ee6b40c4b26226e80
ministrov <61511030+ministrov@users.noreply.github.com>
2025-07-05 14:31:55 +0300

    refactore - Refactore typingEffect component to optimaze a rerender of component for perfomance

--- src/components/typingEffect/TypingEffect.jsx
+++ src/components/typingEffect/TypingEffect.jsx
@@ -1,22 +1,42 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import './style.css';
 
 const TypingEffect = ({ text, speed }) => {
   const [displayedText, setDisplayedText] = useState('');
-  const [index, setIndex] = useState(0);
+  const indexRef = useRef(0);
+  const textRef = useRef(text);
+  const speedRef = useRef(speed);
+
+  // console.log(indexRef);
+  // console.log(textRef);
+  // console.log(speedRef);
 
   useEffect(() => {
+    textRef.current = text;
+    speedRef.current = speed;
+    indexRef.current = 0;
+    setDisplayedText('')
+  }, [text, speed]);
+
+  console.log(displayedText);
+
+  useEffect(() => {
+    if (!text) return;
+
     const interval = setInterval(() => {
-      if (index < text.length) {
-        setDisplayedText((prev) => prev + text[index]);
-        setIndex(index + 1);
+      const currentText = textRef.current;
+      const currentIndex = indexRef.current;
+
+      if (currentIndex < currentText.length) {
+        setDisplayedText(prev => prev + currentText[currentIndex]);
+        indexRef.current += 1;
       } else {
         clearInterval(interval);
       }
-    }, speed);
+    }, speedRef.current);
 
     return () => clearInterval(interval);
-  }, [index, text, speed]);
+  }, [text]);
 
   return (
     <span className="typewriter-text">{displayedText}</span>
