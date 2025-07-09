64b3cf6641c0e7f0aeefd8022e66d9967caedfa4
ministrov <61511030+ministrov@users.noreply.github.com>
2025-07-09 12:56:58 +0300

    fix - Fixing a faq section a11y accessibility

--- src/components/showcasingCard/ShowcasingCard.jsx
+++ src/components/showcasingCard/ShowcasingCard.jsx
@@ -7,8 +7,6 @@ const ShowcasingCard = ({ image, name }) => {
       <div className="showcasing-card__aspect-ratio">
         <img
           src={image}
-          // width="100%"
-          // height="100%"
           className="showcasing-card__image"
           alt={`Focusing on project ${name} illustration`}
           loading="lazy"
