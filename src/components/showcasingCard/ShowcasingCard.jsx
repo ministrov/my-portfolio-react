05a20b5a9693c3c7f690a4cd8cb257eafccfa260
ministrov <61511030+ministrov@users.noreply.github.com>
2025-07-09 12:43:13 +0300

    fix - Fixing an aspect ration problem in showcasing component

--- src/components/showcasingCard/ShowcasingCard.jsx
+++ src/components/showcasingCard/ShowcasingCard.jsx
@@ -3,15 +3,18 @@ import './style.css';
 const ShowcasingCard = ({ image, name }) => {
   return (
     <article className="showcasing-card">
-      <img
-        src={image}
-        width="100%"
-        height="100%"
-        className="showcasing-card__image"
-        alt={`Focusing on project ${name} illustration`}
-        loading="lazy"
-        decoding='async'
-      />
+      {/* Контейнер с фиксированным соотношением сторон */}
+      <div className="showcasing-card__aspect-ratio">
+        <img
+          src={image}
+          // width="100%"
+          // height="100%"
+          className="showcasing-card__image"
+          alt={`Focusing on project ${name} illustration`}
+          loading="lazy"
+          decoding='async'
+        />
+      </div>
     </article>
   );
 };
