5a3b53ac1179c99c8176a0429f6c2fc913cea152
ministrov <61511030+ministrov@users.noreply.github.com>
2025-07-09 10:45:14 +0300

    refactore - Add a code spliting with a lazy loading components and with Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,43 @@
-import { useEffect } from 'react';
+import { useEffect, lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import Layout from './Layout';
 import ScrollToTop from './components/scrollToTop/ScrollToTop';
 import HomePage from './pages/HomePage';
-import ProjectsPage from './pages/ProjectsPage';
-import PageNotFound from './pages/pageNotFound/PageNotFound';
+// import ProjectsPage from './pages/ProjectsPage';
+// import PageNotFound from './pages/pageNotFound/PageNotFound';
 import './styles/main.css';
+
+const ProjectsPage = lazy(() => import('./pages/ProjectsPage'));
+const PageNotFound = lazy(() => import('./pages/pageNotFound/PageNotFound'));
 
 const App = () => {
   const { i18n } = useTranslation();
 
   useEffect(() => {
-    if (i18n) {
+    if (i18n?.isInitialized) {
       document.documentElement.lang = i18n.language;
     }
-  }, [i18n, i18n?.language]);
+  }, [i18n?.isInitialized, i18n.language]);
 
   return (
     <BrowserRouter>
       <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route
-            path="/projects"
-            element={<ProjectsPage/>}
-          />
-          <Route
-            path="*"
-            element={<PageNotFound/>}
-          />
-        </Route>
-      </Routes>
+      <Suspense fallback={null}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route
+              path="/projects"
+              element={<ProjectsPage />}
+            />
+            <Route
+              path="*"
+              element={<PageNotFound />}
+            />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
