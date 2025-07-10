0b5424df8511ef61f4f957556c9f5b95cdbee0a0
ministrov <61511030+ministrov@users.noreply.github.com>
2025-07-10 10:37:58 +0300

    fix - Add an actual my cv pdf file and fix statistics value

--- src/sections/statistics/statistics.js
+++ src/sections/statistics/statistics.js
@@ -1,5 +1,5 @@
 export const statistics = [
-  { id: 1, num: 2, text: 'statistics.years_experience' },
+  { id: 1, num: 4, text: 'statistics.years_experience' },
   { id: 2, num: 16, text: 'statistics.projects_completed' },
   { id: 3, num: 12, text: 'statistics.technologies_mastered' },
   { id: 4, num: 2948, text: 'statistics.code_commits' },
