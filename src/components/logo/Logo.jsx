11f90aa7c65a488e2e3ef3be5f7a7fa4ea1250e4
ministrov <61511030+ministrov@users.noreply.github.com>
2025-07-16 19:58:49 +0300

    fix - Fixing a logo component

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,13 +1,12 @@
 import { Link } from 'react-router-dom';
-import logo from '../../assets/png/logo_bg.png';
+import logo from './logo.png';
 import './style.css';
 
 const Logo = ({ className = '' }) => {
   return (
     <div className={'logo'}>
       <Link to="/" className={`logo__link ${className}`}>
-        {/* {'AntoshkinDEV'} */}
-        <img src={logo} width={148} height={48} alt="Logo of Anton Zhilin" />
+        <img src={logo} width={248} height={48} alt="Logo of Anton Zhilin" />
       </Link>
     </div>
   );
