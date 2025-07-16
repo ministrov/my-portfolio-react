d3ed18963041b8b48760ebf3d450fab81b80c65b
ministrov <61511030+ministrov@users.noreply.github.com>
2025-07-16 12:33:31 +0300

    refactore - Uninstall a react count lib and delete from skill component

--- src/components/skill/Skill.jsx
+++ src/components/skill/Skill.jsx
@@ -1,4 +1,4 @@
-import CountUp from 'react-countup';
+// import CountUp from 'react-countup';
 import './style.css';
 
 const Skill = ({ skill }) => {
@@ -8,18 +8,16 @@ const Skill = ({ skill }) => {
       className="skill__card"
       aria-label={skill.tech}
     >
-      <div className="skill__left">
-        <div className="skill__card-icon">{skill.icon}</div>
-        <span className="skill__card-header">{skill.tech}</span>
-      </div>
+      <div className="skill__card-icon">{skill.icon}</div>
+      <span className="skill__card-header">{skill.tech}</span>
 
-      <p className="skill__count">
+      {/* <p className="skill__count">
         <CountUp
           end={skill.percent}
           duration={5}
           delay={2}
         />%
-      </p>
+      </p> */}
     </div>
   );
 }
