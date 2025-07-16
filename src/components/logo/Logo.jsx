b3fa752de46cad2de41f966b48ca26f6a85633ad
ministrov <61511030+ministrov@users.noreply.github.com>
2025-07-16 18:16:36 +0300

    fix - Fixing a img png into logo component

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
+import logo from '../../assets/png/logo_bg.png';
 import './style.css';
 
-const Logo = ({ className = null }) => {
+const Logo = ({ className = '' }) => {
   return (
     <div className={'logo'}>
       <Link to="/" className={`logo__link ${className}`}>
-        {'AntoshkinDEV'}
+        {/* {'AntoshkinDEV'} */}
+        <img src={logo} width={148} height={48} alt="Logo of Anton Zhilin" />
       </Link>
     </div>
   );
