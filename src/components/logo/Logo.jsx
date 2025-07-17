018e17dbbde8c4a8559d96714d563ce2d711b48d
ministrov <61511030+ministrov@users.noreply.github.com>
2025-07-17 11:04:05 +0300

    refactore - Creating a better logo

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,12 +1,11 @@
 import { Link } from 'react-router-dom';
-import logo from './logo.png';
 import './style.css';
 
 const Logo = ({ className = '' }) => {
   return (
     <div className={'logo'}>
       <Link to="/" className={`logo__link ${className}`}>
-        <img src={logo} width={248} height={48} alt="Logo of Anton Zhilin" />
+        {'AntoshkinDev'}
       </Link>
     </div>
   );
