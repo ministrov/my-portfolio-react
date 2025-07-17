fa5b63414416fb1aebe70685900271d240169682
ministrov <61511030+ministrov@users.noreply.github.com>
2025-07-17 11:32:48 +0300

    feature - Keep on creating a new logo styling

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,9 +1,11 @@
+import { VscTerminal } from "react-icons/vsc";
 import { Link } from 'react-router-dom';
 import './style.css';
 
 const Logo = ({ className = '' }) => {
   return (
     <div className={'logo'}>
+      <VscTerminal className="logo__icon" size={36} color={'#0058a7'} />
       <Link to="/" className={`logo__link ${className}`}>
         {'AntoshkinDev'}
       </Link>
