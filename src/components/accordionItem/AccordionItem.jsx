d0e0924cc961b4c6c7934ca046767e3c41175419
ministrov <61511030+ministrov@users.noreply.github.com>
2025-07-19 13:12:44 +0300

    refactore - Keep on operating on refactoring faq, creating a accordionItem component

---
+++ src/components/accordionItem/AccordionItem.jsx
@@ -0,0 +1,63 @@
+import { motion } from 'framer-motion';
+import { useTranslation } from 'react-i18next';
+import AccordionItemContext from './AccordionItemContext';
+// AnimatePresence
+
+const AccordionItem = ({ item, index, children }) => {
+  const { t } = useTranslation();
+
+  return (
+    <AccordionItemContext.Provider value={{ index }}>
+      <li
+        // className={`faq__item ${activeIndex === index ? 'faq__item--active' : ''
+        //   }`}
+        // key={item.id}
+      >
+        <motion.div
+          className="faq__question"
+        >
+          <h3>{t(item.question)}</h3>
+
+          {/* <motion.button
+            onClick={() => toggleFAQ(index)}
+            className="faq__icon"
+            animate={{
+              rotate: activeIndex === index ? "45deg" : "0deg",
+              transition: { duration: 0.3, ease: "easeInOut" }
+            }}
+            aria-label={activeIndex === index ? t('faqs.close') : t('faqs.open')}
+            aria-expanded={activeIndex === index}
+            aria-controls="dropdown-menu"
+          >
+            <FaPlus />
+          </motion.button> */}
+        </motion.div>
+
+        {/* <AnimatePresence>
+          {activeIndex === index && (
+            <motion.div
+              id="dropdown-menu"
+              className="faq__answer"
+              initial={{ opacity: 0, height: 0, padding: 0 }}
+              animate={{
+                opacity: 1,
+                height: 'auto',
+                paddingTop: 0,
+                paddingLeft: 0,
+                paddingRight: 0,
+                paddingBottom: 24
+              }}
+              exit={{ opacity: 0, height: 0, padding: 0 }}
+              transition={{ duration: 1, ease: 'easeInOut' }}
+            >
+              {t(item.answer)}
+            </motion.div>
+          )}
+        </AnimatePresence> */}
+      </li>
+      {children}
+    </AccordionItemContext.Provider>
+  );
+};
+
+export default AccordionItem;
--- src/sections/faq/Faq.jsx
+++ src/sections/faq/Faq.jsx
@@ -1,6 +1,5 @@
 // import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-// import { motion, AnimatePresence } from 'framer-motion';
 import Accordion from '../../components/accordion/Accordion';
 import Heading from '../../components/heading/Heading';
 // import { FaPlus } from 'react-icons/fa6';
@@ -29,57 +28,13 @@ const Faq = () => {
         />
 
         {/* {faqs.map((item, index) => (
-          <li
-            className={`faq__item ${activeIndex === index ? 'faq__item--active' : ''
-              }`}
-            key={item.id}
-          >
-            <motion.div
-              className="faq__question"
-            >
-              <h3>{t(item.question)}</h3>
 
-              <motion.button
-                onClick={() => toggleFAQ(index)}
-                className="faq__icon"
-                animate={{
-                  rotate: activeIndex === index ? "45deg" : "0deg",
-                  transition: { duration: 0.3, ease: "easeInOut" }
-                }}
-                aria-label={activeIndex === index ? t('faqs.close') : t('faqs.open')}
-                aria-expanded={activeIndex === index}
-                aria-controls="dropdown-menu"
-              >
-                <FaPlus />
-              </motion.button>
-            </motion.div>
-
-            <AnimatePresence>
-              {activeIndex === index && (
-                <motion.div
-                  id="dropdown-menu"
-                  className="faq__answer"
-                  initial={{ opacity: 0, height: 0, padding: 0 }}
-                  animate={{
-                    opacity: 1,
-                    height: 'auto',
-                    paddingTop: 0,
-                    paddingLeft: 0,
-                    paddingRight: 0,
-                    paddingBottom: 24
-                  }}
-                  exit={{ opacity: 0, height: 0, padding: 0 }}
-                  transition={{ duration: 1, ease: 'easeInOut' }}
-                >
-                  {t(item.answer)}
-                </motion.div>
-              )}
-            </AnimatePresence>
-          </li>
         ))} */}
 
 
-        <Accordion/>
+        <Accordion>
+
+        </Accordion>
       </div>
     </section>
   );
