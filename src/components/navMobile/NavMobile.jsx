f0da98caf2157d8b3a9c78f5137cf520bcf56a5a
ministrov <61511030+ministrov@users.noreply.github.com>
2025-07-31 14:30:10 +0300

    fix - Fixing a navMobile to use a location obj when user click link to close menu

--- src/components/navMobile/NavMobile.jsx
+++ src/components/navMobile/NavMobile.jsx
@@ -14,10 +14,6 @@ const NavMobile = () => {
   const location = useLocation();
 
   useEffect(() => {
-    console.log(location);
-  }, [location]);
-
-  useEffect(() => {
     if (isOpen) {
       document.body.style.overflow = 'hidden';
     } else {
@@ -27,7 +23,11 @@ const NavMobile = () => {
 
   useEffect(() => {
     setOpen(false);
-  }, []);
+  }, [location.pathname]);
+
+  // useEffect(() => {
+  //   setOpen(false);
+  // }, []);
 
   return (
     <div className="nav-mobile" ref={ref}>
