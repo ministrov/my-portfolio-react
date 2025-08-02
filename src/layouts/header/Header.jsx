a395dfa921a18c2faef3c76a8a00510f04d7f7ce
ministrov <61511030+ministrov@users.noreply.github.com>
2025-08-02 11:31:22 +0300

    fix - Fixing a header, homePage and main styles

--- src/layouts/header/Header.jsx
+++ src/layouts/header/Header.jsx
@@ -4,25 +4,6 @@ import Logo from '../../components/logo/Logo';
 import './style.css';
 
 const Header = () => {
-  // const increament = (value) => {
-  //   console.log('Function called', value);
-  //   return value + 1;
-  // };
-
-  // const memo = (originalFn) => {
-  //   const result = new Map();
-  //   return (value) => {
-  //     if (!result.has(value)) {
-  //       result.set(value, originalFn(value));
-  //     }
-  //     return result.get(value);
-  //   };
-  // };
-
-  // const memorizedIncreament = memo(increament);
-
-  // console.log(memorizedIncreament(10));
-  // console.log(memorizedIncreament(10));
   return (
     <header className="header">
       <nav className="header__nav nav">
