5f527f1bb20d2a134fa57bb38dbc7219f74883d2
ministrov <61511030+ministrov@users.noreply.github.com>
2025-08-14 16:06:43 +0300

    feat: create a new mobile version of the renewal projectCard

--- src/components/accordionItem/AccordionItem.jsx
+++ src/components/accordionItem/AccordionItem.jsx
@@ -1,4 +1,3 @@
-// import { motion, AnimatePresence } from 'framer-motion';
 import { useTranslation } from 'react-i18next';
 import AccordionButton from '../accordionButton/AccordionButton';
 import AccordionPanel from '../accordionPanel/AccordionPanel';
@@ -10,14 +9,6 @@ const AccordionItem = ({ item, isActive, onClick }) => {
   return (
     <li
       className="faq__item"
-    // initial={{ opacity: 0, height: 0 }}
-    // animate={{
-    //   opacity: 1,
-    //   height: 'auto',
-    //   // paddingBottom: 24
-    // }}
-    // exit={{ opacity: 0, height: 0 }}
-    // transition={{ duration: 0.5, ease: 'easeInOut' }}
     >
       <div
         className="faq__question"
