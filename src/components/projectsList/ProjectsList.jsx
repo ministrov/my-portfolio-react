3b8899059c50a8d1e88d2ed2cc611e668c45c79c
ministrov <61511030+ministrov@users.noreply.github.com>
2025-08-15 14:22:09 +0300

    feat: end up operating with a projectCard animation with framer motion hook useInView

--- src/components/projectsList/ProjectsList.jsx
+++ src/components/projectsList/ProjectsList.jsx
@@ -1,28 +1,46 @@
+import { useEffect, useRef } from 'react';
+import { useInView, motion } from 'framer-motion';
 import { useTranslation } from 'react-i18next';
 import ProjectCard from '../projectCard/ProjectCard';
 import './style.css';
 
 const ProjectsList = ({ projects }) => {
   const { t } = useTranslation();
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  useEffect(() => {
+    console.log(isInView);
+  }, [isInView]);
+
+  const variants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
 
   return (
-    <ul className="projects__list">
-      {projects.map((project) => {
+    <ul className="projects__list" ref={ref}>
+      {projects.map((project, index) => {
         const { id, title, overview, skills, ...rest } = project;
 
         return (
-          <li
+          <motion.li
+            key={id}
+            ref={ref}
             className="project-card__item"
+            variants={variants}
+            initial="initial"
+            animate={isInView ? 'animate' : 'initial'}
+            transition={{ duration: 0.5, delay: index * 0.6, ease: 'easeInOut' }}
             tabIndex={0}
           >
             <ProjectCard
-              key={id}
               title={t(title)}
               overview={t(overview)}
               skills={skills ? skills.split(',') : []}
               {...rest}
             />
-          </li>
+          </motion.li>
         )
       })}
     </ul>
