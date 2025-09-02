43f53f0478cd3062c64cbf091cb277693b58508e
Zhila <ministrov2018@gmail.com>
2025-09-02 07:35:27 +0300

    fix: switch off a modal appearance on homePage component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,66 +1,66 @@
-import { useState, useEffect, useRef } from 'react';
+// import { useState, useEffect, useRef } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
 import Promo from '../sections/promo/Promo';
 import About from '../sections/about/About';
-import Modal from '../components/modal/Modal';
+// import Modal from '../components/modal/Modal';
 import Skills from '../sections/skills/Skills.jsx';
 import Showcasing from '../sections/showcasing/Showcasing.jsx';
 import Faq from '../sections/faq/Faq';
 import Services from '../sections/services/Services.jsx';
 
 const Home = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isModalShown, setIsModalShown] = useState(false);
-  const scrollTriggered = useRef(false);
-  const timeoutRef = useRef(null);
-  const autoCloseRef = useRef(null);
+  // const [isOpen, setIsOpen] = useState(false);
+  // const [isModalShown, setIsModalShown] = useState(false);
+  // const scrollTriggered = useRef(false);
+  // const timeoutRef = useRef(null);
+  // const autoCloseRef = useRef(null);
   const { t } = useTranslation();
 
-  useEffect(() => {
-    if (isOpen) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = 'auto';
-    }
+  // useEffect(() => {
+  //   if (isOpen) {
+  //     document.body.style.overflow = 'hidden';
+  //   } else {
+  //     document.body.style.overflow = 'auto';
+  //   }
 
-    return () => {
-      document.body.style.overflow = 'auto';
-    };
-  }, [isOpen]);
+  //   return () => {
+  //     document.body.style.overflow = 'auto';
+  //   };
+  // }, [isOpen]);
 
-  useEffect(() => {
-    if (isModalShown) return;
+  // useEffect(() => {
+  //   if (isModalShown) return;
 
-    timeoutRef.current = setTimeout(() => {
-      if (!scrollTriggered.current && !isModalShown && !isOpen) {
-        openModal();
-      }
-    }, 30000);
+  //   timeoutRef.current = setTimeout(() => {
+  //     if (!scrollTriggered.current && !isModalShown && !isOpen) {
+  //       openModal();
+  //     }
+  //   }, 30000);
 
-    return () => {
-      clearTimeout(timeoutRef.current);
-      clearTimeout(autoCloseRef.current);
-    };
+  //   return () => {
+  //     clearTimeout(timeoutRef.current);
+  //     clearTimeout(autoCloseRef.current);
+  //   };
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isModalShown, isOpen]);
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [isModalShown, isOpen]);
 
-  const openModal = () => {
-    if (isModalShown || isOpen) return;
+  // const openModal = () => {
+  //   if (isModalShown || isOpen) return;
 
-    setIsOpen(true);
+  //   setIsOpen(true);
 
-    autoCloseRef.current = setTimeout(() => {
-      closeModal();
-    }, 15000);
-  };
+  //   autoCloseRef.current = setTimeout(() => {
+  //     closeModal();
+  //   }, 15000);
+  // };
 
-  const closeModal = () => {
-    setIsOpen(false);
-    setIsModalShown(true);
-    clearTimeout(autoCloseRef.current);
-  };
+  // const closeModal = () => {
+  //   setIsOpen(false);
+  //   setIsModalShown(true);
+  //   clearTimeout(autoCloseRef.current);
+  // };
 
   return (
     <>
@@ -81,11 +81,11 @@ const Home = () => {
       <Skills />
       <Showcasing />
       <Faq />
-      <Modal
+      {/* <Modal
         open={isOpen}
         onClose={() => setIsOpen(false)}
         autoCloseDelay={15000}
-      />
+      /> */}
     </>
   );
 };
