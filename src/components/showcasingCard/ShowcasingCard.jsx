b83189cad1e1f00c504de3de74d296ad9fb08dcd
Zhila <ministrov2018@gmail.com>
2025-09-30 15:01:20 +0300

    refactore: rebuild styling showcasing component to show a projects image nicely

--- src/components/showcasingCard/ShowcasingCard.jsx
+++ src/components/showcasingCard/ShowcasingCard.jsx
@@ -6,10 +6,12 @@ const ShowcasingCard = ({ image, name }) => {
       <div className="showcasing-card__aspect-ratio">
         <img
           src={image}
+          width={1200}
+          height={500}
           className="showcasing-card__image"
           alt={`Focusing on project ${name} illustration`}
           loading="lazy"
-          decoding='async'
+          decoding="async"
         />
       </div>
     </article>
