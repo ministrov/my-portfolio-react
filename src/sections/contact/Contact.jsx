235030f857a44c622ac8db5b43d7b03b5df66f99
Zhila <ministrov2018@gmail.com>
2025-10-01 12:22:04 +0300

    fix: fixing a container div to add to contact section

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -9,17 +9,19 @@ const Contact = () => {
   const { t } = useTranslation();
   return (
     <section className="contact">
-      <Heading
-        title={'Connect with me'}
-        slogan={
-          'Schedule a quick call to learn how Area can turn your regional data into a powerful advantage.'
-        }
-      />
+      <div className="container">
+        <Heading
+          title={'Connect with me'}
+          slogan={
+            'Schedule a quick call to learn how Area can turn your regional data into a powerful advantage.'
+          }
+        />
 
-      <div className="contact__btn-container">
-        <button className="contact__btn" onClick={() => setIsOpen(true)}>
-          {t('footer.reachOutButton')}
-        </button>
+        <div className="contact__btn-container">
+          <button className="contact__btn" onClick={() => setIsOpen(true)}>
+            {t('footer.reachOutButton')}
+          </button>
+        </div>
       </div>
 
       <Modal
