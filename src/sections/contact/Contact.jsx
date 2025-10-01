00753a03a02edacec2b53ecc25272e35af6d2661
Zhila <ministrov2018@gmail.com>
2025-10-01 10:58:38 +0300

    feat: create and add a new section contact

---
+++ src/sections/contact/Contact.jsx
@@ -0,0 +1,18 @@
+import Heading from '../../components/heading/Heading';
+import './style.css';
+
+const Contact = () => {
+  return (
+    <section className="contact">
+      <Heading
+        title={'Connect with me'}
+        slogan={
+          'Schedule a quick call to learn how Area can turn your regional data into a powerful advantage.'
+        }
+      />
+      Contact
+    </section>
+  );
+};
+
+export default Contact;
