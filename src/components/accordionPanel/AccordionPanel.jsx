c2be93927dad63ff64634e096e8e23fce47562c6
Zhila <ministrov2018@gmail.com>
2025-10-02 16:00:59 +0300

    feat: create a new realisation for accordion panel appearing with no framer motion component with a pure css

--- src/components/accordionPanel/AccordionPanel.jsx
+++ src/components/accordionPanel/AccordionPanel.jsx
@@ -1,4 +1,3 @@
-import { motion, AnimatePresence } from 'framer-motion';
 import { useTranslation } from 'react-i18next';
 import './style.css';
 
@@ -6,24 +5,9 @@ const AccordionPanel = ({ item, isOpen }) => {
   const { t } = useTranslation();
 
   return (
-    <AnimatePresence mode="wait">
-      {isOpen && (
-        <motion.div
-          key="accordion-content"
-          className="faq__answer"
-          initial={{ opacity: 0, height: 0 }}
-          animate={{ opacity: 1, height: 'auto' }}
-          exit={{
-            opacity: 0,
-            height: 0,
-            transition: { duration: 0.5, ease: 'easeInOut' },
-          }}
-          transition={{ duration: 0.5, ease: 'easeInOut' }}
-        >
-          {item.answer && t(item.answer)}
-        </motion.div>
-      )}
-    </AnimatePresence>
+    <div className={`faq__answer ${isOpen ? 'open' : ''}`}>
+      {item.answer && t(item.answer)}
+    </div>
   );
 };
 
