3ac6340d942b2e38cad39a5055db7b6e09c55b17
Zhila <ministrov2018@gmail.com>
2026-01-27 15:45:15 +0300

    feat: end up creating a modal promo component

--- src/components/modalPromo/ModalPromo.jsx
+++ src/components/modalPromo/ModalPromo.jsx
@@ -1,7 +1,19 @@
+import fire from '../../assets/svg/fire.svg';
 import './style.css';
 
 const ModalPromo = () => {
-  return <div className="modal__promo modal-promo">Разработка -50%</div>;
+  return (
+    <div className="modal__promo modal-promo">
+      <img
+        className="modal-promo__icon"
+        src={fire}
+        width={35}
+        height={47}
+        alt=""
+      />
+      Разработка -50%
+    </div>
+  );
 };
 
 export default ModalPromo;
