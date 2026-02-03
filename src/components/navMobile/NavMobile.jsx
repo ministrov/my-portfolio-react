09feae1143cea7966c2e4e1e38f6ffb10789a89f
Zhila <ministrov2018@gmail.com>
2026-02-03 14:26:49 +0300

    fix: fixing a mobile version of a navMobile component

--- src/components/navMobile/NavMobile.jsx
+++ src/components/navMobile/NavMobile.jsx
@@ -1,10 +1,10 @@
-import { useState, useRef, useEffect } from "react";
-import { useLocation, NavLink } from "react-router-dom";
-import { Squash as Hamburger } from "hamburger-react";
-import { AnimatePresence, motion } from "framer-motion";
-import { useTranslation } from "react-i18next";
-import ToggleLang from "../toggleLang/ToggleLang";
-import { routes } from "../../const";
+import { useState, useRef, useEffect } from 'react';
+import { useLocation, NavLink } from 'react-router-dom';
+import { Squash as Hamburger } from 'hamburger-react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useTranslation } from 'react-i18next';
+import ToggleLang from '../toggleLang/ToggleLang';
+import { routes } from '../../const';
 import './style.css';
 
 const NavMobile = () => {
@@ -29,20 +29,16 @@ const NavMobile = () => {
     <div className="nav-mobile" ref={ref}>
       <div className="nav-mobile__buttons">
         <div className="nav-mobile__lang">
-          <ToggleLang
-            aria-label={t("Change language")}
-          />
+          <ToggleLang aria-label={t('Change language')} />
         </div>
-        <div className="nav-mobile__hamburger">
-          <Hamburger
-            toggled={isOpen}
-            size={30}
-            toggle={setOpen}
-            color="#ffffff"
-            hideOutline={false}
-            label="Show menu"
-          />
-        </div>
+        <Hamburger
+          toggled={isOpen}
+          size={30}
+          toggle={setOpen}
+          color="#0058a7"
+          hideOutline={false}
+          label="Show menu"
+        />
       </div>
 
       <AnimatePresence>
@@ -51,7 +47,7 @@ const NavMobile = () => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            transition={{ duration: 0.5, ease: "easeInOut" }}
+            transition={{ duration: 0.5, ease: 'easeInOut' }}
             tabIndex="-1"
             className="nav-mobile__wrapper"
           >
@@ -64,7 +60,7 @@ const NavMobile = () => {
                     initial={{ scale: 0, opacity: 0 }}
                     animate={{ scale: 1, opacity: 1 }}
                     transition={{
-                      type: "spring",
+                      type: 'spring',
                       stiffness: 240,
                       damping: 28,
                       delay: 0.4 + idx / 5,
@@ -77,12 +73,12 @@ const NavMobile = () => {
                     <NavLink
                       to={route.href}
                       onClick={() => setOpen((prev) => !prev)}
-                      className={
-                        "nav-mobile__link"
-                      }
-                      aria-label={`${t(route.title)} ${t("Go to page")}`}
+                      className={'nav-mobile__link'}
+                      aria-label={`${t(route.title)} ${t('Go to page')}`}
                     >
-                      <span className="nav-mobile__title">{t(route.title)}</span>
+                      <span className="nav-mobile__title">
+                        {t(route.title)}
+                      </span>
                       <Icon className="nav-mobile__icon" color="#0058a7" />
                     </NavLink>
                   </motion.li>
