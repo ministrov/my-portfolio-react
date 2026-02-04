af80e626f276e31190f830e6f745627b6fa7d4ad
Zhila <ministrov2018@gmail.com>
2026-02-04 11:59:19 +0300

    feat: start creating a new about section

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion';
 import { useTranslation } from 'react-i18next';
-import author from '../../assets/png/photo.webp';
+// import author from '../../assets/png/photo.webp';
 import Heading from '../../components/heading/Heading';
 import './style.css';
 
@@ -10,33 +10,35 @@ const About = () => {
   return (
     <section className="about">
       <div className="container">
-        <Heading
-          title={t('heading.about.name')}
-          slogan={t('heading.about.subheading')}
-          className="about__title"
-        ></Heading>
-
         <div className="about__wrapper">
-          <motion.div
+          <Heading
+            title={t('heading.about.name')}
+            // slogan={t('heading.about.subheading')}
+            className="about__title"
+          ></Heading>
+          {/* <motion.div
             className="about__left"
-            initial={{ opacity: 0, scale: 0.8, x: -30 }} // Уменьшил масштаб и добавил смещение по X
+            initial={{ opacity: 0, scale: 0.8, x: -30 }}
             whileInView={{ opacity: 1, scale: 1, x: 0 }}
-            viewport={{ margin: '-50px' }} // Добавил настройки viewport
+            viewport={{ margin: '-50px' }}
             transition={{
-              duration: 0.8, // Увеличил длительность
-              ease: [0.25, 0.1, 0.25, 1], // Более плавная easing-функция
-              delay: 0.2, // Добавил небольшую задержку
+              duration: 0.8,
+              ease: [0.25, 0.1, 0.25, 1],
+              delay: 0.2,
             }}
           >
-            <img
+            
+            
+          </motion.div> */}
+
+          {/* <img
               className="about__image"
               src={author}
               width={500}
               height={500}
               alt={"Focus on author's face"}
               loading="lazy"
-            />
-          </motion.div>
+            /> */}
 
           <motion.div
             className="about__right"
