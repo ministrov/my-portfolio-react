59bb7f579953850d870527af71bdcd12c955b895
Zhila <ministrov2018@gmail.com>
2026-02-05 18:09:04 +0300

    feat: create an about experience section component structure

---
+++ src/components/aboutExperience/AboutExperience.jsx
@@ -0,0 +1,13 @@
+import Heading from '../heading/Heading';
+import './style.css';
+
+const AboutExperience = () => {
+  return (
+    <section className="about-experience">
+      <Heading>AboutExperience</Heading>
+      <div>AboutExperience</div>
+    </section>
+  );
+};
+
+export default AboutExperience;
