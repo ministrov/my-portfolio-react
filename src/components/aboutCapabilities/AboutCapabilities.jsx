afda881af24fe2f1dd8617633e5724f588ef7a0f
Zhila <ministrov2018@gmail.com>
2026-02-05 18:05:50 +0300

    feat: start creaing a sections component within an about page

---
+++ src/components/aboutCapabilities/AboutCapabilities.jsx
@@ -0,0 +1,16 @@
+import Heading from '../heading/Heading';
+import './style.css';
+
+const AboutCapabilities = () => {
+  return (
+    <section className="about-capabilities">
+      <div className="container">
+        <Heading>AboutCapabilities</Heading>
+
+        <div>AboutCapabilities</div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutCapabilities;
