9a63aa7dfba44889646dfafdc0cc772c6ecb7067
Zhila <ministrov2018@gmail.com>
2026-02-06 11:03:24 +0300

    feat: testing an example of looking an about experience component

--- src/components/aboutExperience/AboutExperience.jsx
+++ src/components/aboutExperience/AboutExperience.jsx
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 import Heading from '../heading/Heading';
 import './style.css';
 
 const AboutExperience = () => {
+  const [isBlack, setIsBlack] = useState(false);
+
   return (
-    <section className="about-experience">
+    <section
+      className={`about-experience ${isBlack ? 'about-experience--dark' : ''}`}
+    >
       <div className="container">
         <Heading title={'My Experience'} />
 
         <div>AboutExperience</div>
+
+        <button onClick={() => setIsBlack((prev) => !prev)} type="button">
+          Сменить фон
+        </button>
       </div>
     </section>
   );
