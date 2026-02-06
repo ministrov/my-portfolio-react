c372ad8d61723bad1ff2395b83f91e3a86052118
Zhila <ministrov2018@gmail.com>
2026-02-06 13:58:48 +0300

    feat: keep on creating an about capabilities section

--- src/components/aboutCapabilities/AboutCapabilities.jsx
+++ src/components/aboutCapabilities/AboutCapabilities.jsx
@@ -8,7 +8,13 @@ const AboutCapabilities = () => {
         <div className="about-capabilities__wrapper">
           <Heading title={'My Capabilities'} />
 
-          <div>My Capabilities</div>
+          <div className="about-capabilites__text">
+            <p>
+              I am always looking to add more skills.Morbi egestas neque eu
+              blandit fermentum. Nulla ac lobortis ligula. Pellentesque ac ex at
+              purus faucibus tristique ut et dolor.{' '}
+            </p>
+          </div>
         </div>
       </div>
     </section>
