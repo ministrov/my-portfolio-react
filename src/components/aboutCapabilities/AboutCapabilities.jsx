1a769e30cf711494bd21ba6d4c025a3da7a48447
Zhila <ministrov2018@gmail.com>
2026-02-06 16:22:08 +0300

    feat: end up creating an about capabilities section with translation on Desktop viewpor

--- src/components/aboutCapabilities/AboutCapabilities.jsx
+++ src/components/aboutCapabilities/AboutCapabilities.jsx
@@ -1,21 +1,37 @@
 import { useTranslation } from 'react-i18next';
 import Heading from '../heading/Heading';
+import Tag from '../tag/Tag';
 import './style.css';
 
 const AboutCapabilities = () => {
   const { t } = useTranslation();
+  const skills = [
+    'JavaScript',
+    'JQuery',
+    'HTML',
+    'CSS',
+    'React',
+    'RTK',
+    'Zustand',
+    'Next',
+  ];
+
   return (
     <section className="about-capabilities">
       <div className="container">
         <div className="about-capabilities__wrapper">
           <Heading title={t('heading.capabilities.name')} />
 
-          <div className="about-capabilites__text">
-            <p>
-              I am always looking to add more skills.Morbi egestas neque eu
-              blandit fermentum. Nulla ac lobortis ligula. Pellentesque ac ex at
-              purus faucibus tristique ut et dolor.{' '}
+          <div className="about-capabilities__text-container">
+            <p className="about-capabilities__text">
+              {t('aboutCapabilities.text')}{' '}
             </p>
+
+            <ul className="about-capabilites__list">
+              {skills.map((skill) => (
+                <Tag key={skill}>{skill}</Tag>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
