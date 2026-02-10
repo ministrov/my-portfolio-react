0312f02b9b93039290e1c90fd3c21b87ac59f3a8
Zhila <ministrov2018@gmail.com>
2026-02-10 12:28:08 +0300

    feat: create an inhanced semantic markup for experience section

--- src/components/aboutExperience/AboutExperience.jsx
+++ src/components/aboutExperience/AboutExperience.jsx
@@ -11,32 +11,68 @@ const AboutExperience = () => {
         <div className="about-experience__wrapper">
           <Heading title={t('heading.experience.name')} />
 
-          <div className="about-experience__experience">
-            <div className="about-experience__item">
-              <header className="about-experience__head">
-                <h3 className="about-experience__title">Freelance Developer</h3>
-
-                <data value="Sep 2023 — Nov 2023 ">Sep 2023 — Nov 2023</data>
-              </header>
-              <p className="about-experience__item-text">
-                Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-                aute irure dolor in reprehenderit in voluptate velit esse cillum
-                dolore eu fugiat nulla pariatur.
-              </p>
-            </div>
-            <div className="about-experience__item">
-              <header className="about-experience__head">
-                <h3 className="about-experience__title">Front-End Intern</h3>
-
-                <data value="Sep 2023 — Nov 2023">Nov 2023 — Present</data>
-              </header>
-              <p className="about-experience__item-text">
-                Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-                aute irure dolor in reprehenderit in voluptate velit esse cillum
-                dolore eu fugiat nulla pariatur.
-              </p>
-            </div>
-          </div>
+          <ul className="about-experience__experience-list">
+            <li>
+              <article className="about-experience__item">
+                <header className="about-experience__head">
+                  <h3 className="about-experience__title">
+                    Freelance Developer
+                  </h3>
+                  <time
+                    className="about-experience__date"
+                    datetime="2023-09/2023-11"
+                  >
+                    Sep 2021 — Nov 2023
+                  </time>
+                </header>
+                <p className="about-experience__item-text">
+                  Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+                  aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur.
+                </p>
+              </article>
+            </li>
+            <li>
+              <article className="about-experience__item">
+                <header className="about-experience__head">
+                  <h3 className="about-experience__title">
+                    Frontend Developer Intern
+                  </h3>
+                  <time
+                    className="about-experience__date"
+                    datetime="2023-09/2023-11"
+                  >
+                    Nov 2023 — Nov 2024
+                  </time>
+                </header>
+                <p className="about-experience__item-text">
+                  Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+                  aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur.
+                </p>
+              </article>
+            </li>
+            <li>
+              <article className="about-experience__item">
+                <header className="about-experience__head">
+                  <h3 className="about-experience__title">
+                    Frontend Developer
+                  </h3>
+                  <time
+                    className="about-experience__date"
+                    datetime="2023-09/2023-11"
+                  >
+                    Nov 2024 — Present
+                  </time>
+                </header>
+                <p className="about-experience__item-text">
+                  Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+                  aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur.
+                </p>
+              </article>
+            </li>
+          </ul>
         </div>
       </div>
     </section>
