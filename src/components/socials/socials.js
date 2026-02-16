fdd321bb591d924cba658e061adc49d63c2eddec
Zhila <ministrov2018@gmail.com>
2026-02-16 15:21:54 +0300

    fix: delete the whatsapp social icon from socials component

--- src/components/socials/socials.js
+++ src/components/socials/socials.js
@@ -1,9 +1,11 @@
-import { FaSquareWhatsapp } from 'react-icons/fa6';
 import { FaGithub, FaTelegram, FaVk } from 'react-icons/fa';
 
 export const socials = [
   { icon: <FaGithub />, name: 'github', path: 'https://github.com/ministrov' },
-  { icon: <FaTelegram />, name: 'telegram', path: 'https://t.me/antonzhilin83' },
+  {
+    icon: <FaTelegram />,
+    name: 'telegram',
+    path: 'https://t.me/antonzhilin83',
+  },
   { icon: <FaVk />, name: 'vk', path: 'https://vk.com/feed' },
-  { icon: <FaSquareWhatsapp />, name: 'whatsapp', path: 'https://t.me/antonzhilin83' },
 ];
