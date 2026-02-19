ca42c8454ccc6cdd5c55f5e32d64fc9e91b20dc3
Zhila <ministrov2018@gmail.com>
2026-02-19 12:54:54 +0300

    refactore: refactore an advantages component to reduce a code amount to decomposite for an AdvantagesList and AdvantagesItem

--- src/sections/advantages/Advantages.jsx
+++ src/sections/advantages/Advantages.jsx
@@ -1,10 +1,8 @@
-import { useTranslation, Trans } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import Heading from '../../components/heading/Heading';
-import website from '../../assets/svg/website.svg';
-import deploy from '../../assets/png/deploy.png';
-import reactIcon from '../../assets/svg/react-js.svg';
-import seo from '../../assets/png/seo.png';
-import bug from '../../assets/png/bag.png';
+import AdvantagesList from './AdvantagesList';
+import AdvantagesItem from './AdvantagesItem';
+import itemList from './items';
 import './style.css';
 
 const Advantages = () => {
@@ -17,83 +15,16 @@ const Advantages = () => {
           slogan={t('heading.advantages.subheading')}
         />
 
-        <ul className="advantages__list">
-          <li className="advantages__item">
-            <img
-              className="advantages__item-img"
-              src={website}
-              width="56"
-              height="56"
-              alt=""
+        <AdvantagesList>
+          {itemList.map((item) => (
+            <AdvantagesItem
+              key={item.id}
+              text={item.text}
+              icon={item.icon}
+              altText={item.alt}
             />
-            <p className="advantages__item-text">
-              <Trans
-                i18nKey="advantages.items.first"
-                components={{ hightlight: <strong /> }}
-              />
-            </p>
-          </li>
-          <li className="advantages__item">
-            <img
-              className="advantages__item-img"
-              src={deploy}
-              width="56"
-              height="56"
-              alt=""
-            />
-            <p className="advantages__item-text">
-              <Trans
-                i18nKey="advantages.items.second"
-                components={{ hightlight: <strong /> }}
-              />
-            </p>
-          </li>
-          <li className="advantages__item">
-            <img
-              className="advantages__item-img"
-              src={reactIcon}
-              width="56"
-              height="56"
-              alt=""
-            />
-            <p className="advantages__item-text">
-              <Trans
-                i18nKey="advantages.items.third"
-                components={{ hightlight: <strong /> }}
-              />
-            </p>
-          </li>
-          <li className="advantages__item">
-            <img
-              className="advantages__item-img"
-              src={seo}
-              width="56"
-              height="56"
-              alt=""
-            />
-            <p className="advantages__item-text">
-              <Trans
-                i18nKey="advantages.items.fourth"
-                components={{ hightlight: <strong /> }}
-              />
-            </p>
-          </li>
-          <li className="advantages__item">
-            <img
-              className="advantages__item-img"
-              src={bug}
-              width="56"
-              height="56"
-              alt=""
-            />
-            <p className="advantages__item-text">
-              <Trans
-                i18nKey="advantages.items.fifth"
-                components={{ hightlight: <strong /> }}
-              />
-            </p>
-          </li>
-        </ul>
+          ))}
+        </AdvantagesList>
       </div>
     </section>
   );
