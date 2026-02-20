85a5eb9428fe2cf6e149595a082e7e00ed7598db
Zhila <ministrov2018@gmail.com>
2026-02-20 15:42:40 +0300

    refactore: for ProjectsList use `import { LazyMotion, m } from "framer-motion"` with `domAnimation` features — saves ~30kb

--- src/components/accordionButton/AccordionButton.jsx
+++ src/components/accordionButton/AccordionButton.jsx
@@ -28,32 +28,3 @@ const AccordionButton = ({ isActive, id }) => {
 };
 
 export default AccordionButton;
-
-// import { motion } from 'framer-motion';
-// import { FaPlus } from 'react-icons/fa6';
-// import './style.css';
-
-// const AccordionButton = ({ isActive, id }) => {
-//   const iconVariants = {
-//     open: { rotate: 45 },
-//     closed: { rotate: 0 },
-//   };
-//   const ariaLabel = isActive ? 'Collapse section' : 'Expand section';
-
-//   return (
-//     <motion.button
-//       type="button"
-//       className="faq__button"
-//       variants={iconVariants}
-//       animate={isActive ? 'open' : 'closed'}
-//       transition={{ duration: 0.4 }}
-//       aria-expanded={isActive ? 'true' : 'false'}
-//       aria-controls={`panel-${id}`}
-//       aria-label={ariaLabel}
-//     >
-//       <FaPlus />
-//     </motion.button>
-//   );
-// };
-
-// export default AccordionButton;
