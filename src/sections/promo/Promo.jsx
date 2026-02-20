0b0ff0732bc98f4e2ecfaf1bb7fa9da3975479fb
Zhila <ministrov2018@gmail.com>
2026-02-20 14:28:02 +0300

    refactore: for Promo component use `import { LazyMotion, m } from "framer-motion"` with `domAnimation` features — saves ~30kb

--- src/sections/promo/Promo.jsx
+++ src/sections/promo/Promo.jsx
@@ -1,5 +1,5 @@
 import { useTranslation } from 'react-i18next';
-import { motion } from 'framer-motion';
+import { LazyMotion, m, domAnimation } from 'framer-motion';
 import { BsBoxArrowInUpRight } from 'react-icons/bs';
 import { TypeAnimation } from 'react-type-animation';
 import { useLanguage } from '../../context/LanguageProvider';
@@ -35,95 +35,105 @@ const Promo = () => {
       </h2>
       <div className="container">
         <div className="promo__wrapper">
-          <div className="promo__text">
-            <motion.strong
-              className="promo__greeting"
-              {...commonAnimation}
-              transition={{
-                ...commonAnimation.transition,
-                delay: 0.2,
-              }}
-            >
-              {lang === 'ru' ? 'Привет, Я' : "Hi, I'm"}
-              {<br />}
-              {lang === 'ru' && (
-                <TypeAnimation
-                  sequence={['Антон Жилин', 1000, 'Frontend Developer', 1000]}
-                  speed={50}
-                  repeat={Infinity}
-                />
-              )}
-              {lang === 'en' && (
-                <TypeAnimation
-                  sequence={['Anton Zhilin', 1000, 'Frontend Developer', 1000]}
-                  speed={50}
-                  repeat={Infinity}
-                />
-              )}
-              .
-            </motion.strong>
+          <LazyMotion features={domAnimation}>
+            <div className="promo__text">
+              <m.strong
+                className="promo__greeting"
+                {...commonAnimation}
+                transition={{
+                  ...commonAnimation.transition,
+                  delay: 0.2,
+                }}
+              >
+                {lang === 'ru' ? 'Привет, Я' : "Hi, I'm"}
+                {<br />}
+                {lang === 'ru' && (
+                  <TypeAnimation
+                    sequence={['Антон Жилин', 1000, 'Frontend Developer', 1000]}
+                    speed={50}
+                    repeat={Infinity}
+                  />
+                )}
+                {lang === 'en' && (
+                  <TypeAnimation
+                    sequence={[
+                      'Anton Zhilin',
+                      1000,
+                      'Frontend Developer',
+                      1000,
+                    ]}
+                    speed={50}
+                    repeat={Infinity}
+                  />
+                )}
+                .
+              </m.strong>
 
-            <motion.p
-              className="promo__slogan"
+              <m.p
+                className="promo__slogan"
+                {...commonAnimation}
+                transition={{
+                  ...commonAnimation.transition,
+                  delay: 0.4,
+                }}
+              >
+                {t('promo.promoSlogan')}
+              </m.p>
+
+              <m.div
+                className="promo__btns"
+                {...commonAnimation}
+                transition={{
+                  ...commonAnimation.transition,
+                  delay: 0.6,
+                }}
+              >
+                <a
+                  className={'promo__btn'}
+                  href={cvPdf}
+                  download={cvPdf}
+                  rel="noopener noreferrer"
+                >
+                  {t('promo.promoBtn')}
+                  <span className="btn__icon">
+                    <BsBoxArrowInUpRight width={20} height={20} />
+                  </span>
+                </a>
+
+                <SocialList variant="blue" />
+              </m.div>
+            </div>
+
+            <m.div
+              key="promo-image"
+              className="promo__image"
               {...commonAnimation}
               transition={{
                 ...commonAnimation.transition,
                 delay: 0.4,
               }}
             >
-              {t('promo.promoSlogan')}
-            </motion.p>
-
-            <motion.div
-              className="promo__btns"
-              {...commonAnimation}
-              transition={{
-                ...commonAnimation.transition,
-                delay: 0.6,
-              }}
-            >
-              <a
-                className={'promo__btn'}
-                href={cvPdf}
-                download={cvPdf}
-                rel="noopener noreferrer"
-              >
-                {t('promo.promoBtn')}
-                <span className="btn__icon">
-                  <BsBoxArrowInUpRight width={20} height={20} />
-                </span>
-              </a>
-
-              <SocialList variant="blue" />
-            </motion.div>
-          </div>
-
-          <motion.div
-            key="promo-image"
-            className="promo__image"
-            {...commonAnimation}
-            transition={{
-              ...commonAnimation.transition,
-              delay: 0.4,
-            }}
-          >
-            <picture>
-              <source
-                srcSet={`${avatar3x} 1500w`}
-                media="(min-width: 1025px)"
-              />
-              <source srcSet={`${avatar2x} 1000w`} media="(min-width: 769px)" />
-              <source srcSet={`${avatar} 500w`} media="(max-width: 768px)" />
-              <img
-                className="promo__avatar"
-                src={avatar}
-                width="500"
-                height="500"
-                alt="Avatar a pixel man with a laptop"
-                fetchpriority="true"
-              />
-            </picture>
-          </motion.div>
+              <picture>
+                <source
+                  srcSet={`${avatar3x} 1500w`}
+                  media="(min-width: 1025px)"
+                />
+                <source
+                  srcSet={`${avatar2x} 1000w`}
+                  media="(min-width: 769px)"
+                />
+                <source srcSet={`${avatar} 500w`} media="(max-width: 768px)" />
+                <img
+                  className="promo__avatar"
+                  src={avatar}
+                  width="500"
+                  height="500"
+                  alt="Avatar a pixel man with a laptop"
+                  fetchpriority="high"
+                />
+              </picture>
+            </m.div>
+          </LazyMotion>
         </div>
       </div>
       <MouseScroll />
@@ -132,3 +142,138 @@ const Promo = () => {
 };
 
 export default Promo;
+
+// import { useTranslation } from 'react-i18next';
+// import { motion } from 'framer-motion';
+// import { BsBoxArrowInUpRight } from 'react-icons/bs';
+// import { TypeAnimation } from 'react-type-animation';
+// import { useLanguage } from '../../context/LanguageProvider';
+// import SocialList from '../../components/socials/SocialList';
+// import MouseScroll from '../../components/mouseScroll/MouseScroll';
+// import avatar from '../../assets/png/my-avatar.webp';
+// import avatar2x from '../../assets/png/my-avatar-1000.webp';
+// import avatar3x from '../../assets/png/my-avatar-1500.webp';
+// import cvPdf from '../../assets/pdfs/my-cv.pdf';
+// import './style.css';
+
+// const commonAnimation = {
+//   initial: { opacity: 0, y: 50 },
+//   whileInView: {
+//     opacity: 1,
+//     y: 0,
+//   },
+//   viewport: { margin: '-50px' },
+//   transition: {
+//     duration: 0.8,
+//     ease: [0.25, 0.1, 0.25, 1],
+//   },
+// };
+
+// const Promo = () => {
+//   const { lang } = useLanguage();
+//   const { t } = useTranslation();
+
+//   return (
+//     <section className="promo">
+//       <h2 className="visually-hidden">
+//         A promo section for introduction of the author
+//       </h2>
+//       <div className="container">
+//         <div className="promo__wrapper">
+//           <div className="promo__text">
+//             <motion.strong
+//               className="promo__greeting"
+//               {...commonAnimation}
+//               transition={{
+//                 ...commonAnimation.transition,
+//                 delay: 0.2,
+//               }}
+//             >
+//               {lang === 'ru' ? 'Привет, Я' : "Hi, I'm"}
+//               {<br />}
+//               {lang === 'ru' && (
+//                 <TypeAnimation
+//                   sequence={['Антон Жилин', 1000, 'Frontend Developer', 1000]}
+//                   speed={50}
+//                   repeat={Infinity}
+//                 />
+//               )}
+//               {lang === 'en' && (
+//                 <TypeAnimation
+//                   sequence={['Anton Zhilin', 1000, 'Frontend Developer', 1000]}
+//                   speed={50}
+//                   repeat={Infinity}
+//                 />
+//               )}
+//               .
+//             </motion.strong>
+
+//             <motion.p
+//               className="promo__slogan"
+//               {...commonAnimation}
+//               transition={{
+//                 ...commonAnimation.transition,
+//                 delay: 0.4,
+//               }}
+//             >
+//               {t('promo.promoSlogan')}
+//             </motion.p>
+
+//             <motion.div
+//               className="promo__btns"
+//               {...commonAnimation}
+//               transition={{
+//                 ...commonAnimation.transition,
+//                 delay: 0.6,
+//               }}
+//             >
+//               <a
+//                 className={'promo__btn'}
+//                 href={cvPdf}
+//                 download={cvPdf}
+//                 rel="noopener noreferrer"
+//               >
+//                 {t('promo.promoBtn')}
+//                 <span className="btn__icon">
+//                   <BsBoxArrowInUpRight width={20} height={20} />
+//                 </span>
+//               </a>
+
+//               <SocialList variant="blue" />
+//             </motion.div>
+//           </div>
+
+//           <motion.div
+//             key="promo-image"
+//             className="promo__image"
+//             {...commonAnimation}
+//             transition={{
+//               ...commonAnimation.transition,
+//               delay: 0.4,
+//             }}
+//           >
+//             <picture>
+//               <source
+//                 srcSet={`${avatar3x} 1500w`}
+//                 media="(min-width: 1025px)"
+//               />
+//               <source srcSet={`${avatar2x} 1000w`} media="(min-width: 769px)" />
+//               <source srcSet={`${avatar} 500w`} media="(max-width: 768px)" />
+//               <img
+//                 className="promo__avatar"
+//                 src={avatar}
+//                 width="500"
+//                 height="500"
+//                 alt="Avatar a pixel man with a laptop"
+//                 fetchpriority="true"
+//               />
+//             </picture>
+//           </motion.div>
+//         </div>
+//       </div>
+//       <MouseScroll />
+//     </section>
+//   );
+// };
+
+// export default Promo;
