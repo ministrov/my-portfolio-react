bf8120dcb61afb432ed4c9fbf5818d4ffd7927a9
Zhila <ministrov2018@gmail.com>
2026-02-20 14:05:10 +0300

    refactore: for Skills component use `import { LazyMotion, m } from "framer-motion"` with `domAnimation` features — saves ~30kb

--- src/sections/skills/Skills.jsx
+++ src/sections/skills/Skills.jsx
@@ -1,9 +1,8 @@
 import { useTranslation } from 'react-i18next';
-import { motion } from 'framer-motion';
+import { LazyMotion, m, domAnimation } from 'framer-motion';
 import Heading from '../../components/heading/Heading';
 import Skill from '../../components/skill/Skill';
-import { itemsArrayForLeft } from './skills';
-import { itemsArrayForRight } from './skills';
+import { itemsArrayForLeft, itemsArrayForRight } from './skills';
 import './style.css';
 
 import 'swiper/css';
@@ -39,36 +38,117 @@ const Skills = () => {
           title={t('heading.mySkills.name')}
           slogan={t('heading.mySkills.subheading')}
           className="skills__title"
-        ></Heading>
+        />
       </div>
 
       <div className="skills__list-container">
-        <motion.ul
-          className="skills__list"
-          style={{ display: 'flex', width: '200%' }}
-          {...animationConfigRight}
-        >
-          {[...itemsArrayForRight, ...itemsArrayForRight].map((skill) => (
-            <li key={skill.id + Math.random()}>
-              <Skill skill={skill} />
-            </li>
-          ))}
-        </motion.ul>
+        <LazyMotion features={domAnimation}>
+          <m.ul
+            className="skills__list"
+            style={{ display: 'flex', width: '200%' }}
+            {...animationConfigRight}
+          >
+            {[...itemsArrayForRight, ...itemsArrayForRight].map(
+              (skill, index) => (
+                <li key={`${skill.id}-${index}`}>
+                  <Skill skill={skill} />
+                </li>
+              )
+            )}
+          </m.ul>
 
-        <motion.ul
-          className="skills__list"
-          style={{ display: 'flex', width: '200%' }}
-          {...animationConfigLeft}
-        >
-          {[...itemsArrayForLeft, ...itemsArrayForLeft].map((skill) => (
-            <li key={skill.id + Math.random()}>
-              <Skill skill={skill} />
-            </li>
-          ))}
-        </motion.ul>
+          <m.ul
+            className="skills__list"
+            style={{ display: 'flex', width: '200%' }}
+            {...animationConfigLeft}
+          >
+            {[...itemsArrayForLeft, ...itemsArrayForLeft].map(
+              (skill, index) => (
+                <li key={`${skill.id}-${index}`}>
+                  <Skill skill={skill} />
+                </li>
+              )
+            )}
+          </m.ul>
+        </LazyMotion>
       </div>
     </section>
   );
 };
 
 export default Skills;
+
+// import { useTranslation } from 'react-i18next';
+// import { motion } from 'framer-motion';
+// import Heading from '../../components/heading/Heading';
+// import Skill from '../../components/skill/Skill';
+// import { itemsArrayForLeft } from './skills';
+// import { itemsArrayForRight } from './skills';
+// import './style.css';
+
+// import 'swiper/css';
+// import 'swiper/css/a11y';
+
+// const Skills = () => {
+//   const { t } = useTranslation();
+
+//   const animationConfigRight = {
+//     initial: { x: '0%' },
+//     animate: { x: '-50%' },
+//     transition: {
+//       repeat: Infinity,
+//       ease: 'linear',
+//       duration: 25,
+//     },
+//   };
+
+//   const animationConfigLeft = {
+//     initial: { x: '-50%' },
+//     animate: { x: '0%' },
+//     transition: {
+//       repeat: Infinity,
+//       ease: 'linear',
+//       duration: 25,
+//     },
+//   };
+
+//   return (
+//     <section className="skills">
+//       <div className="container">
+//         <Heading
+//           title={t('heading.mySkills.name')}
+//           slogan={t('heading.mySkills.subheading')}
+//           className="skills__title"
+//         ></Heading>
+//       </div>
+
+//       <div className="skills__list-container">
+//         <motion.ul
+//           className="skills__list"
+//           style={{ display: 'flex', width: '200%' }}
+//           {...animationConfigRight}
+//         >
+//           {[...itemsArrayForRight, ...itemsArrayForRight].map((skill) => (
+//             <li key={skill.id + Math.random()}>
+//               <Skill skill={skill} />
+//             </li>
+//           ))}
+//         </motion.ul>
+
+//         <motion.ul
+//           className="skills__list"
+//           style={{ display: 'flex', width: '200%' }}
+//           {...animationConfigLeft}
+//         >
+//           {[...itemsArrayForLeft, ...itemsArrayForLeft].map((skill) => (
+//             <li key={skill.id + Math.random()}>
+//               <Skill skill={skill} />
+//             </li>
+//           ))}
+//         </motion.ul>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Skills;
