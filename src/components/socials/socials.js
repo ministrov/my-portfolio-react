023deee231079b238a8e8cb0a2490c2300e8c5d6
Zhila <ministrov2018@gmail.com>
2026-05-14 10:27:40 +0300

    feat(socials): add a new icon to test

--- src/components/socials/socials.js
+++ src/components/socials/socials.js
@@ -1,4 +1,4 @@
-import { FaGithub, FaTelegram, FaVk } from 'react-icons/fa';
+import { FaGithub, FaTelegram, FaVk, FaAccessibleIcon } from 'react-icons/fa';
 
 export const socials = [
   { icon: <FaGithub />, name: 'github', path: 'https://github.com/ministrov' },
@@ -8,4 +8,9 @@ export const socials = [
     path: 'https://t.me/antonzhilin83',
   },
   { icon: <FaVk />, name: 'vk', path: 'https://vk.com/feed' },
+  {
+    icon: <FaAccessibleIcon />,
+    name: 'max',
+    path: 'https://max.ru/u/f9LHodD0cOIXiSCiy_gZjqb3BsPHkg0KDk2xXBBaOm5UvY2EpobTT2EppB0',
+  },
 ];
